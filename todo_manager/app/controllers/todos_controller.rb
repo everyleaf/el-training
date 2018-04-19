9b7b35d6107e08d078efb8f68bed038e2c0d13d8
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-19 17:37:46 +0900

    make new page and create action

--- todo_manager/app/controllers/todos_controller.rb
+++ todo_manager/app/controllers/todos_controller.rb
@@ -2,4 +2,15 @@ class TodosController < ApplicationController
   def index
 		@todos = Todo.all
   end
+	
+	def new
+	end
+
+	def create
+		@todo = Todo.new
+		@todo.title = params[:title]
+		@todo.content = params[:content]
+		@todo.save
+		redirect_to('/todos/index')
+	end
 end
--- todo_manager/config/routes.rb
+++ todo_manager/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'todos/index'
+	get 'todos/new'
+	post 'todos/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
