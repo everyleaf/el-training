e6be8547985639921a432648c810ded8e2145e2f
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-21 00:11:49 +0900

    change tab to 2 spaces

--- todo_manager/app/controllers/todos_controller.rb
+++ todo_manager/app/controllers/todos_controller.rb
@@ -1,48 +1,48 @@
 class TodosController < ApplicationController
   def index
-		@todos = Todo.all
+    @todos = Todo.all
   end
-	
-	def new
-		@todo = Todo.new
-	end
 
-	def create
-		@todo = Todo.new
-		@todo.title = params[:title]
-		@todo.content = params[:content]
-		if @todo.save
-			flash[:notice] = "New todo has been created."
-			redirect_to('/')
-		else
-			render 'new'
-		end
-	end
+  def new
+    @todo = Todo.new
+  end
 
-	def detail
-		@todo = Todo.find_by(id: params[:id])
-	end
+  def create
+    @todo = Todo.new
+    @todo.title = params[:title]
+    @todo.content = params[:content]
+    if @todo.save
+      flash[:notice] = "New todo has been created."
+      redirect_to('/')
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		@todo = Todo.find_by(id: params[:id])
-	end	
+  def detail
+    @todo = Todo.find_by(id: params[:id])
+  end
 
-	def update
-		@todo = Todo.find_by(id: params[:id])
-		@todo.title = params[:title]
-		@todo.content = params[:content]
-		if @todo.save
-			flash[:notice] = "Todo has been updated."
-			redirect_to("/todos/#{@todo.id}/detail")
-		else
-			render 'edit'
-		end
-	end
+  def edit
+    @todo = Todo.find_by(id: params[:id])
+  end
 
-	def destroy
-		@todo = Todo.find_by(id: params[:id])
-		@todo.destroy
-		flash[:notice] = "Todo has been deleted."
-		redirect_to("/")
-	end
+  def update
+    @todo = Todo.find_by(id: params[:id])
+    @todo.title = params[:title]
+    @todo.content = params[:content]
+    if @todo.save
+      flash[:notice] = "Todo has been updated."
+      redirect_to("/todos/#{@todo.id}/detail")
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @todo = Todo.find_by(id: params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo has been deleted."
+    redirect_to("/")
+  end
 end
