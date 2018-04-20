23b28d94ea197f507e66f4ff82dee4b674dce7f7
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-20 15:00:57 +0900

    add test for new page

--- todo_manager/spec/requests/todos_spec.rb
+++ todo_manager/spec/requests/todos_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Todos", type: :request do
-  describe "Check Home (index) page" do
+  describe "Home (index) page" do
     it "should have the content 'Todo List'" do
       visit '/'
       expect(page).to have_content('Todo List')
     end
   end
+
+	describe "new page" do
+		it "should have the content 'Todo List'" do
+			visit '/todos/new'
+			expect(page).to have_content('Create Todo')
+		end
+	end
 end
