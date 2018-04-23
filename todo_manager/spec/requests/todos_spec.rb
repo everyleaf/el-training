df35926da70353823ba2c52f81165e3127e05dee
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-23 16:12:20 +0900

    write feature spec to all views and actions of todo

--- todo_manager/spec/requests/todos_spec.rb
+++ todo_manager/spec/requests/todos_spec.rb
@@ -1,27 +1,143 @@
 require 'rails_helper'
 
-RSpec.describe "Todos", type: :request do
-  describe "Home (index) page" do
-    it "should have the content 'Todo List'" do
-      visit '/'
+RSpec.describe 'Todos', type: :request do
+  let(:todo) {create(:todo)}
+
+  shared_examples_for 'have a header' do
+    describe 'header' do
+      it 'should have a header and the index link' do
+        expect(page).to have_link('Todo Manager', href: '/')
+      end
+    end
+  end
+
+  describe 'Home (index) page' do
+    before { visit '/' }
+    it_behaves_like 'have a header'
+    it "should have the word 'Todo List'" do
       expect(page).to have_content('Todo List')
     end
-  end
+    it 'should have the create link' do
+      expect(page).to have_link('Create', href: '/todos/new')
+    end
 
-  describe "new page" do
-    it "should have the content 'Todo List'" do
-      visit '/todos/new'
-      expect(page).to have_content('Create Todo')
+    describe 'create page' do
+      before { click_on 'Create' }
+      it_behaves_like 'have a header'
+      it "should have the word 'Create Todo'" do
+        expect(page).to have_content('Create Todo')
+      end
+
+      describe 'create new todo' do
+        context 'title is nil' do
+          before do
+            fill_in 'content', with: 'hoge'
+            click_on 'create'
+          end
+          it 'should back to the create page' do
+            expect(current_path).to eq '/todos/create'
+          end
+          it 'should show a flash message' do
+            expect(page).to have_content("Title can't be blank")
+          end
+          it 'should keep the content' do
+            expect(page).to have_field('content', with: 'hoge')
+          end
+        end
+
+        context 'title is not nil' do
+          before do
+            fill_in 'title', with: 'Sample title'
+            fill_in 'content', with: 'Sample content'
+            click_on 'create'
+          end
+          it_behaves_like 'have a header'
+          it 'should be index page after creating' do
+            expect(current_path).to eq '/'
+          end
+          it 'should show a flash message' do
+            expect(page).to have_content('New todo has been created.')
+          end
+          it 'should show the created todo' do
+            expect(page).to have_link('Sample title')
+            expect(page).to have_content('Sample content')
+          end
+
+          describe 'detail page' do
+            before { click_on 'Sample title' }
+            it_behaves_like 'have a header'
+            it 'should have the title and content of the todo' do
+              expect(page).to have_content('Sample title')
+              expect(page).to have_content('Sample content')
+            end
+            it 'should have the edit and destroy link' do
+              expect(page).to have_link('edit')
+              expect(page).to have_link('destroy')
+            end
+
+            describe 'edit page' do
+              before { click_on 'edit' }
+              it_behaves_like 'have a header'
+              it "should have the content 'Edit Todo', title and content of the todo" do
+                expect(page).to have_content('Edit Todo')
+              end
+              it 'should have the title and content of the todo' do
+                expect(page).to have_field('title', with: 'Sample title')
+                expect(page).to have_field('content', with: 'Sample content')
+              end
+
+              describe 'update the todo' do
+                context 'title is nil' do
+                  before do
+                    fill_in 'title', with: ''
+                    click_on 'update'
+                  end
+                  it_behaves_like 'have a header'
+                  it 'should back to the edit page' do
+                    expect(page).to have_content('Edit Todo')
+                  end
+                  it 'should show a flash message' do
+                    expect(page).to have_content("Title can't be blank")
+                  end
+                  it 'should keep the content' do
+                    expect(page).to have_field('content', with: 'Sample content')
+                  end
+                end
+
+                context 'title is not nil' do
+                  before do
+                    fill_in 'title', with: 'Edited title'
+                    fill_in 'content', with: 'Edited content'
+                    click_on 'update'
+                  end
+                  it_behaves_like 'have a header'
+                  it 'should be detail page after updating' do
+                    expect(current_path).to include "/detail"
+                  end
+                  it 'should show the updated content' do
+                    expect(page).to have_content('Edited title')
+                    expect(page).to have_content('Edited content')
+                  end
+                  it 'should show a flash message' do
+                    expect(page).to have_content('Todo has been updated.')
+                  end
+                end
+              end
+            end
+
+            describe 'destroy action' do
+              before { click_on 'destroy' }
+              it 'should delete todo' do
+                expect(page).to_not have_link('Sample Title')
+                expect(page).to_not have_content('Sample Content')
+              end
+              it 'should show a flash message' do
+                expect(page).to have_content('Todo has been deleted.')
+              end
+            end
+          end
+        end
+      end
     end
   end
-
-  describe "detail page" do
-    it "should have the title and content of the todo" do
-      todo = create(:todo)
-      visit "/todos/#{todo.id}/detail"
-      expect(page).to have_content('Sample title')
-      expect(page).to have_content('Sample content')
-    end
-  end
-
 end
