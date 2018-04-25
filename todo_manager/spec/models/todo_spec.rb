05c5a1f1726353ac68a8645cc9bc1d7d4335dac9
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-25 11:01:12 +0900

    change create method from FactoryBot to ActiveRecord, add line break between end and context and remove unnecessary line break

--- todo_manager/spec/models/todo_spec.rb
+++ todo_manager/spec/models/todo_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe Todo, type: :model do
     describe 'title' do
       context 'is nil' do
         it "can't be created" do
-          expect{ create(:todo, title: '') }.to raise_error(ActiveRecord::RecordInvalid)
+          expect{ Todo.create!(title: '') }.to raise_error(ActiveRecord::RecordInvalid)
         end
       end
+
       context 'is not nil' do
         it 'can be created' do
-          expect{ create(:todo) }.not_to raise_error
+          expect{ Todo.create!(title: 'hoge') }.not_to raise_error
         end
       end
     end
   end
-    
 end
