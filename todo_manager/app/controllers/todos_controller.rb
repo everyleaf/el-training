11b6c01252ce6757cbdb5e5902d706d84684232f
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-26 14:57:35 +0900

    add deadline to views and controller

--- todo_manager/app/controllers/todos_controller.rb
+++ todo_manager/app/controllers/todos_controller.rb
@@ -1,14 +1,19 @@
 class TodosController < ApplicationController
   def index
-    @todos = Todo.all.order(created_at: :desc)
+    if params[:sort].nil?
+      @todos = Todo.all.order(created_at: :desc)
+    else
+      #hoge
+    end
   end
 
   def new
     @todo = Todo.new
+    @todo.deadline = Time.now.tomorrow.strftime('%Y-%m-%dT%H:%M')
   end
 
   def create
-    @todo = Todo.new(title: params[:title], content: params[:content])
+    @todo = Todo.new(title: params[:title], content: params[:content], deadline: params[:deadline])
     if @todo.save
       flash[:notice] = I18n.t('flash.todos.create')
       redirect_to('/')
@@ -29,6 +34,7 @@ class TodosController < ApplicationController
     @todo = Todo.find_by(id: params[:id])
     @todo.title = params[:title]
     @todo.content = params[:content]
+    @todo.deadline = params[:deadline]
     if @todo.save
       flash[:notice] = I18n.t('flash.todos.update')
       redirect_to("/todos/#{@todo.id}/detail")
