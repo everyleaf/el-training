ff940e39b8ca26a599468534c8311a9faf1ea87a
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-04-27 09:57:30 +0900

    add status_id column to todos

---
+++ todo_manager/db/migrate/20180426081744_add_status_to_todos.rb
@@ -0,0 +1,5 @@
+class AddStatusToTodos < ActiveRecord::Migration[5.2]
+  def change
+    add_column :todos, :status_id, :integer, null: false, default: 0
+  end
+end
--- todo_manager/db/schema.rb
+++ todo_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_051342) do
+ActiveRecord::Schema.define(version: 2018_04_26_081744) do
 
   create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_04_25_051342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline"
+    t.integer "status_id", default: 0, null: false
   end
 
 end
