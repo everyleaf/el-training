7700b1fac25d4460e56c6170b40f6f6772ec29ea
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-05-02 16:07:56 +0900

    add kaminari to index page

--- todo_manager/app/controllers/todos_controller.rb
+++ todo_manager/app/controllers/todos_controller.rb
@@ -5,7 +5,7 @@ class TodosController < ApplicationController
     todos = Todo.where('title like ? and status_id like ?', "%#{@search}%", @status)
     @direction = ['asc', 'desc'].include?(params[:direction]) ? params[:direction] : 'desc'
     @sort = ['created_at', 'deadline', 'priority_id'].include?(params[:sort]) ? params[:sort] : 'created_at'
-    @todos = todos.order("#{@sort} #{@direction}")
+    @todos = todos.page(params[:page]).per(10).order("#{@sort} #{@direction}")
     render 'index'
   end
 
