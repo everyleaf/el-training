239420a4bbbe70c73c63211800d5e19cc6e3d30f
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-05-10 17:18:43 +0900

    modify seeds to create another user

--- todo_manager/db/seeds.rb
+++ todo_manager/db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: 'Kotaro', password: '100012845', user_type: 0)
+User.create(name: 'hoge', password: 'fuga', user_type: 1)
