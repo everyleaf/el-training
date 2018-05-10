d9ceeea44ad6f7a9622a495ebb55f87e59a76fe6
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-05-10 12:28:35 +0900

    add association between user and todos to factory

--- todo_manager/spec/factories/todos.rb
+++ todo_manager/spec/factories/todos.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     title 'Sample title'
     content 'Sample content'
     deadline 1.day.since
+
+    association :user, factory: :user, name: 'hoge', password: 'fuga'
   end
 end
