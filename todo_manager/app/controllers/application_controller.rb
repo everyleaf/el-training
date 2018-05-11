47ea6371c41e8f5a43ec3b02ba680d8d6fc0c846
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-05-11 16:47:16 +0900

    add current_user method to application_helper and remove set_current_user from application_controller, use nil? method, remove unnecessary braces

--- todo_manager/app/controllers/application_controller.rb
+++ todo_manager/app/controllers/application_controller.rb
@@ -1,19 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
-
-  def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
-  end
+  include ApplicationHelper
 
   def authenticate_user
-    if @current_user == nil
+    if current_user.nil?
       flash[:notice] = I18n.t('flash.users.login.must')
       redirect_to('/login')
     end
   end
 
   def forbid_login_user
-    if @current_user
+    if current_user
       flash[:notice] = I18n.t('flash.users.login.already')
       redirect_to('/')
     end
--- todo_manager/app/helpers/application_helper.rb
+++ todo_manager/app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
