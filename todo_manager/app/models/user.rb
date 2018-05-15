51c64c32db3953f7776a925de6b6c1a0efb0af0e
Kotaro Nishioka <kotaro.nishioka@rakuten.com>
2018-05-15 10:12:40 +0900

    reformat code and add enum to user Model

--- todo_manager/app/models/user.rb
+++ todo_manager/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, {presence: true, uniqueness: true}
+  validates :name, { presence: true, uniqueness: true }
+  enum user_type: { admin: 0, normal: 1 }
   has_secure_password
   has_many :todos, dependent: :destroy
 end
