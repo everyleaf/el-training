5a97d7288e349071427e86bd7adfd88de6f05ed7
KyuSoo Kim <anzel21@naver.com>
2018-09-19 09:35:59 +0900

    [change]factory_bot導入とミスの修正

--- KTask/spec/features/tasks_spec.rb
+++ KTask/spec/features/tasks_spec.rb
@@ -13,13 +13,13 @@ RSpec.feature 'Tasks', type: :feature do
       expect(page).to have_content('Task was successfully create.')
       expect(page).to have_content('First content')
       expect(page).to have_content('Rspec test')
-    end
+    end.to change { Task.count }.by(1)
   end
 
   scenario 'タスクの修正' do
     visit root_path
     click_link '作成'
-    fill_in 'Title', with: 'Before the modify'
+    fill_in 'Title', with: 'Before modify'
     fill_in 'Content', with: 'Rspec test'
     click_button 'Create Task'
     expect do
@@ -30,7 +30,7 @@ RSpec.feature 'Tasks', type: :feature do
       expect(page).to have_content 'Task was successfully updated.'
       expect(page).to have_content 'After modify'
       expect(page).to have_content 'Sleepy morning'
-    end
+    end.to change { Task.count }.by(0)
   end
 
   scenario 'タスクの削除' do
@@ -42,7 +42,6 @@ RSpec.feature 'Tasks', type: :feature do
     expect do
       click_link '削除'
       expect(page).to have_content 'Task was successfully destroyed.'
-    end
+    end.to change { Task.count }.by(-1)
   end
-
 end
