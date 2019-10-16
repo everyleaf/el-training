176add09bbc42bdeb0d389a080976787c66f0617
yu-hasebe <yu_hasebe@fablic.co.jp>
2019-10-16 17:09:54 +0900

    fix based on #429

--- scripts/step6_create_model_and_db_schema.sh
+++ scripts/step6_create_model_and_db_schema.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # user
-rails generate model user name:string login_id:string password:digest deleted_at:datetime
+# rails generate model user name:string login_id:string password:digest deleted_at:datetime
 rails db:migrate
 rails db:migrate:redo
 
 # task
-rails generate model task name:string description:text user:references priority:integer status:integer due:datetime deleted_at:datetime
+# rails generate model task name:string description:text user:references priority:integer status:integer due:datetime deleted_at:datetime
 rails db:migrate
 rails db:migrate:redo
 
 # label
-rails generate model label name:string
+# rails generate model label name:string
 rails db:migrate
 rails db:migrate:redo
 
 # task_label
-rails generate model task_label task:references label:references
+# rails generate model task_label task:references label:references
 rails db:migrate
 rails db:migrate:redo
