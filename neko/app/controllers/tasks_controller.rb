e2d512d66ecf5aeaea1e1f1dd01142130e14bb77
craftzcat <takumi.c.yoshida@rakuten.com>
2020-04-30 14:28:53 +0900

    implement CRUD for Task management

--- neko/app/controllers/tasks_controller.rb
+++ neko/app/controllers/tasks_controller.rb
@@ -1,22 +1,59 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.create(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクを作成しました'
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'タスクの作成に失敗しました'
+      render.new
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'タスクを更新しました'
+      redirect_to task_path(@task)
+    else
+      flash.now[:danger] = 'タスクの更新に失敗しました'
+      render :edit
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+
+    if @task.destroy
+      flash[:success] = 'タスクを削除しました'
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'タスクの削除に失敗しました'
+      render :show
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
--- neko/config/routes.rb
+++ neko/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/show'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'tasks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  root 'tasks#index'
 end
