fc33b21fc2bc1dff58f416f677c0f641b1cdd84e
craftzcat <takumi.c.yoshida@rakuten.com>
2020-04-30 14:50:36 +0900

    modify tasks model

--- neko/db/migrate/20200430011219_create_tasks.rb
+++ neko/db/migrate/20200430011219_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :name, null: false
+      t.string :name, presence: true
       t.text :description
 
       t.timestamps
--- neko/db/schema.rb
+++ neko/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_04_30_011219) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
