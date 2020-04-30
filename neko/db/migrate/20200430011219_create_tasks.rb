e798ccb89dbef32d3f77a974d5225c47c0a021a4
craftzcat <takumi.c.yoshida@rakuten.com>
2020-04-30 15:01:43 +0900

    Revert "modify tasks model"
    This reverts commit fc33b21fc2bc1dff58f416f677c0f641b1cdd84e.

--- neko/db/migrate/20200430011219_create_tasks.rb
+++ neko/db/migrate/20200430011219_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :name, presence: true
+      t.string :name, null: false
       t.text :description
 
       t.timestamps
