bfada17ebc59b207b67237f7303b433eba1bbeb2
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-07 10:01:39 +0900

    minor corrections

--- neko/app/controllers/tasks_controller.rb
+++ neko/app/controllers/tasks_controller.rb
@@ -21,11 +21,9 @@ class TasksController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @task.update(task_params)
