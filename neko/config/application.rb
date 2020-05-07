fa2bbd8bb84386ba0d5eab18f2ea444ff0839603
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-07 10:21:26 +0900

    install gems & configure

--- neko/config/application.rb
+++ neko/config/application.rb
@@ -15,5 +15,18 @@ module Neko
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    config.generators do |g|
+      g.factory_bot false
+      g.javascripts false
+      g.helper false
+      g.factory_bot false
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        routing_specs: false,
+        request_specs: false
+    end
   end
 end
