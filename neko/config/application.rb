fbd0de0642105c3599b55f417e52221bd68da555
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-07 11:04:03 +0900

    install gem & configure

--- neko/config/application.rb
+++ neko/config/application.rb
@@ -28,5 +28,11 @@ module Neko
         routing_specs: false,
         request_specs: false
     end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
