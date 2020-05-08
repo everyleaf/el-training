545fe9e1b4a00d8c63a4832f42bbd662539c54f1
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-08 10:13:38 +0900

    add exception handling

--- neko/app/controllers/application_controller.rb
+++ neko/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  unless Rails.env.development?
+    rescue_from Exception, with: :render500
+    rescue_from ActionController::RoutingError, with: :render404
+    rescue_from ActiveRecord::RecordNotFound, with: :render404
+  end
+
+  def render404(exception = nil)
+    logger.error "Rendering 404 with exception: #{exception.message}" if exception
+    render 'errors/404', status: :not_found
+  end
+
+  def render500(exception = nil)
+    logger.error "Rendering 500 with exception: #{exception.message}" if exception
+    render 'errors/500', status: :internal_server_error
+  end
 end
