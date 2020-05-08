2fe4740b493a04eaa1d72f1dfbdf4c7f45e0505f
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-08 16:28:54 +0900

    arrange tasks & add spec

--- neko/spec/system/task_spec.rb
+++ neko/spec/system/task_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 describe 'task', type: :system do
-  let!(:task) { FactoryBot.create(:task) }
+  let!(:tasks) { FactoryBot.create_list(:task, 5) }
 
   describe '#index' do
     context 'accress root' do
-      it 'should be success to access the task list' do
-        visit tasks_path
+      before { visit tasks_path }
 
+      it 'should be success to access the task list' do
         expect(page).to have_content 'タスク一覧'
         expect(page).to have_content '名前'
         expect(page).to have_content '説明'
+      end
+
+      it 'tasks should be arrange in descending date order' do
+        expect(page.all('.task-name').map(&:text)).to eq tasks.map { |h| h[:name] }.reverse
       end
     end
   end
@@ -32,7 +36,7 @@ describe 'task', type: :system do
   describe '#edit (GET /tasks/:id/edit)' do
     context 'name is more than one letter' do
       it 'should be success' do
-        visit edit_task_path(task.id)
+        visit edit_task_path(tasks[0].id)
 
         fill_in '名前', with: 'hogehoge'
         fill_in '説明', with: 'fugaguga'
@@ -46,11 +50,11 @@ describe 'task', type: :system do
   describe '#show (GET /tasks/:id)' do
     context 'access the detail page' do
       it 'should be success' do
-        visit task_path(task.id)
+        visit task_path(tasks[0].id)
 
         expect(page).to have_content 'タスク詳細'
-        expect(page).to have_content task.name
-        expect(page).to have_content task.description
+        expect(page).to have_content tasks[0].name
+        expect(page).to have_content tasks[0].description
       end
     end
   end
@@ -58,7 +62,7 @@ describe 'task', type: :system do
   describe '#delete (DELETE /tasks/:id)', js: true do
     context 'push delete button from detail page' do
       it 'should be success to delete the task' do
-        visit task_path(task.id)
+        visit task_path(tasks[0].id)
 
         # confirm dialog
         page.accept_confirm do
