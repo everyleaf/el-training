07a288df627e2a0608712e64e215267aa57e3525
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-11 11:17:51 +0900

    add due_at  to controller & view

--- neko/app/controllers/tasks_controller.rb
+++ neko/app/controllers/tasks_controller.rb
@@ -52,6 +52,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :due_at, :have_a_due)
   end
 end
