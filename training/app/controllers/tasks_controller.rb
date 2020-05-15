10a84a8ad68ced9a92d5a712d7d8b93a5ef03aff
tatsuminaoki <naoki.a.tatsumi@rakuten.com>
2020-05-15 14:48:24 +0900

    create tasks CRUD

---
+++ training/app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to tasks_path, notice: '新しいタスクが作成されました'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(tasks_params)
+      redirect_to tasks_path, notice: '新しいタスクが更新されました'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, alert: 'タスクが削除されました'
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :description, :priority, :status, :due_date)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- training/app/models/task.rb
+++ training/app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+  enum priority: {
+    low: 0,
+    medium: 1,
+    hight: 2,
+  }
+
+  enum status: {
+    waiting: 0,
+    working: 1,
+    done: 2
+  }
 end
