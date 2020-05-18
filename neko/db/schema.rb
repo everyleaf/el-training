520f91b4b83e1fd01029cabc0dfa0cba6e4edabf
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-18 09:58:15 +0900

    create migraton & rails db:migrate

--- neko/db/schema.rb
+++ neko/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_011219) do
+ActiveRecord::Schema.define(version: 2020_05_18_004240) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
