20b4070635d0a9b1c424f8966b1177d260a5e002
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-26 16:35:02 +0900

    refactor app/models/task.rb

--- neko/app/models/task.rb
+++ neko/app/models/task.rb
@@ -4,11 +4,10 @@ class Task < ApplicationRecord
   enum status: { not_proceed: 0, in_progress: 1, done: 2 }
 
   def self.rearrange(column, direction)
-    case column
-    when 'due_at'
-      order(have_a_due: :desc).order("tasks.#{column} #{direction}")
+    if column == 'due_at'
+      order(have_a_due: :desc).order("#{column} #{direction}")
     else
-      order("tasks.#{column} #{direction}")
+      order("#{column} #{direction}")
     end
   end
 
@@ -16,11 +15,11 @@ class Task < ApplicationRecord
     if search[:name].blank? && search[:status].blank?
       all
     elsif search[:status].blank?
-      where(['tasks.name LIKE ?', "%#{search[:name]}%"])
+      where(['name LIKE ?', "%#{search[:name]}%"])
     elsif search[:name].blank?
       where(status: search[:status])
     else
-      where(status: search[:status]).where(['tasks.name LIKE ?', "%#{search[:name]}%"])
+      where(status: search[:status]).where(['name LIKE ?', "%#{search[:name]}%"])
     end
   end
 end
