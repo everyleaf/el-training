371dded5c3ee222f64df29ce90f7492aa9f9dc0a
craftzcat <takumi.c.yoshida@rakuten.com>
2020-05-26 10:40:51 +0900

    move initilize_statuses to seed

--- neko/db/seeds.rb
+++ neko/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+statuses = ['未着手', '着手中', '完了']
+statuses.each_with_index do |status, idx|
+  Status.create(name: status, phase: idx)
+end
