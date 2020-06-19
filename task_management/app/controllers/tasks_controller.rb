92a3ed6177c3bdfca41874920c50ce97b08ea966
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-06-19 16:27:48 +0900

    (指摘取込)不要な空行＆空白の削除、及び、重複処理の共通化

--- task_management/app/controllers/tasks_controller.rb
+++ task_management/app/controllers/tasks_controller.rb
@@ -1,40 +1,31 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
-    @task = Task.new 
+    @task = Task.new
   end
-  
+
   def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to tasks_path, notice: 'タスクが登録されました'
-    end
+    @task.save if redirect_to tasks_path, notice: 'タスクが登録されました'
   end
 
   def edit
-    @task = Task.find(params[:id]) 
   end
-  
+
   def update
-    @task = Task.find(params[:id])
-    if @task.update(task_params)
-      redirect_to tasks_path, notice: 'タスクが編集されました'
-    end
-  end  
+    @task.update(task_params) if redirect_to tasks_path, notice: 'タスクが編集されました'
+  end
 
   def destroy
-    @task = Task.find(params[:id])
-    if @task.destroy
-      redirect_to tasks_path, notice: 'タスクを削除しました'
-    end
+    @task.destroy if redirect_to tasks_path, notice: 'タスクを削除しました'
   end
 
   private
@@ -43,4 +34,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :priority, :status, :due, :description)
   end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
