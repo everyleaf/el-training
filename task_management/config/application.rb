e0451dbc69174c1e5f7351d8991cc630f45547fa
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-06-24 09:56:23 +0900

    複数のロケールファイルを読み込めるよう設定を追加

--- task_management/config/application.rb
+++ task_management/config/application.rb
@@ -16,5 +16,6 @@ module TaskManagement
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
