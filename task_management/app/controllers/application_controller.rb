a31d05d92ca458a3de3f7bd1e50000dbf53687c9
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-06-24 12:58:14 +0900

    エラーページの実装

--- task_management/app/controllers/application_controller.rb
+++ task_management/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :render500
+  rescue_from ActionController::RoutingError, with: :render404
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+
+  def render404
+    render 'errors/404', status: :not_found
+  end
+
+  def render500
+    render 'errors/500', status: :internal_server_error
+  end
 end
--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  get '*path', to: 'application#render404'
 end
