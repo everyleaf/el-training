1074a31701baae43bd410f5602b912c31e2a5f54
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-06-24 10:47:57 +0900

    タイムゾーンを東京に設定

--- task_management/config/application.rb
+++ task_management/config/application.rb
@@ -17,5 +17,6 @@ module TaskManagement
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.time_zone = 'Tokyo'
   end
 end
