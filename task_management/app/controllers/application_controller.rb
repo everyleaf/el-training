f5e3e76d12d1c58572426697edabe9e871af2cd4
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-06-25 10:44:33 +0900

    エラー発生時にログを吐くように設定

--- task_management/app/controllers/application_controller.rb
+++ task_management/app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::RoutingError, with: :render404
   rescue_from ActiveRecord::RecordNotFound, with: :render404
 
-  def render404
+  def render404(exception = nil)
+    logger.error "404 error: #{exception.message}" if exception
     render 'errors/404', status: :not_found
   end
 
-  def render500
+  def render500(exception = nil)
+    logger.error "500 error: #{exception.message}" if exception
     render 'errors/500', status: :internal_server_error
   end
 end
