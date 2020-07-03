d736ba7647c036a301187472a8a381f3efa2b0ed
‘satoshi’ <satoshi.kawana@rakuten.com>
2020-07-03 11:18:13 +0900

    バリデーションテストの作成

--- task_management/spec/models/task_spec.rb
+++ task_management/spec/models/task_spec.rb
@@ -2,32 +2,124 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'バリデーション' do
-    let!(:task) { build(:task, title: title) }
+    describe 'タイトル' do
+      subject { task }
 
-    subject { task }
+      let!(:task) { build(:task, title: title) }
 
-    context '入力が正しい場合' do
-      let(:title) { 'TASK' }
+      context '入力が正しい場合' do
+        let(:title) { 'TASK' }
 
-      it { is_expected.to be_valid }
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:title) { '' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '文字数の最大値の場合' do
+        let(:title) { 'a' * 20 }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '文字数の最大値を超過した場合' do
+        let(:title) { 'a' * 21 }
+
+        it { is_expected.to be_invalid }
+      end
     end
 
-    context '空欄の場合' do
-      let(:title) { '' }
+    describe '優先度' do
+      subject { task }
 
-      it { is_expected.to be_invalid }
+      let(:task) { build(:task, priority: priority) }
+
+      context '入力が正しい場合' do
+        let(:priority) { 0 }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:priority) { '' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '数値以外の場合' do
+        let(:priority) { 'PRIORITY' }
+
+        it { expect { subject }.to raise_error(ArgumentError) }
+      end
+
+      context '0~2以外の数値の場合' do
+        let(:priority) { 3 }
+
+        it { expect { subject }.to raise_error(ArgumentError) }
+      end
     end
 
-    context '文字数の最大値の場合' do
-      let(:title) { 'a' * 20 }
+    describe 'ステータス' do
+      subject { task }
 
-      it { is_expected.to be_valid }
+      let(:task) { build(:task, status: status) }
+
+      context '入力が正しい場合' do
+        let(:status) { 0 }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:status) { '' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '数値以外の場合' do
+        let(:status) { 'STATUS' }
+
+        it { expect { subject }.to raise_error(ArgumentError) }
+      end
+
+      context '0~2以外の数値の場合' do
+        let(:status) { '3' }
+
+        it { expect { subject }.to raise_error(ArgumentError) }
+      end
     end
 
-    context '文字数の最大値を超過した場合' do
-      let(:title) { 'a' * 21 }
+    describe '期日' do
+      subject { task }
 
-      it { is_expected.to be_invalid }
+      let!(:task) { build(:task, due: due) }
+
+      context '入力が正しい場合' do
+        let(:due) { '2020/07/02' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '空欄の場合' do
+        let(:due) { '' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '存在しない日付の場合' do
+        let(:due) { '2020/99/99' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '日付のフォーマットでない文字列の場合' do
+        let(:due) { 'AAAAAA' }
+
+        it { is_expected.to be_invalid }
+      end
     end
   end
 end
