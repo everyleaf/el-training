4867ba36547a1a397fb134149ff78ff63db477a2
mkLotto <hiroto.mukai@rakuten.com>
2020-12-15 11:10:30 +0900

    task create controller

---
+++ job_san/app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    id = params[:id]
+    @task = Task.find_by(id: id)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(create_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'タスクができたよ！'
+    else
+      flash.now[:alert] = 'タスクができなかったよ！'
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:task).permit(:name, :description)
+  end
+
+end
