aa3899441d71a82e23747b390f2a677fd22c2d1a
mkLotto <hiroto.mukai@rakuten.com>
2020-12-15 14:21:13 +0900

    commonize error messages

--- job_san/app/controllers/tasks_controller.rb
+++ job_san/app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
 class TasksController < ApplicationController
+
+  TASK_CREATED = 'タスクを作成したよ'.freeze
+  TASK_NOT_CREATED = 'タスクが作成できなかったよ'.freeze
+  TASK_NOT_FOUND = 'そのタスクないよ'.freeze
+  TASK_UPDATED = 'タスクを更新したよ'.freeze
+  TASK_NOT_UPDATED = 'タスクが更新できなかったよ'.freeze
+  TASK_DELETED = 'タスクを削除したよ'.freeze
+  TASK_NOT_DELETED = 'タスクが削除できなかったよ'.freeze
+
   def index
     @tasks = Task.all
   end
@@ -16,38 +25,42 @@ class TasksController < ApplicationController
 
     # TODO: ステップ11までバリデーションは実装しません。
     if @task.save
-      redirect_to tasks_path, notice: 'タスクを作成したよ'
+      redirect_to tasks_path, notice: TASK_CREATED
     else
-      flash.now[:alert] = 'タスクが作成できなかったよ'
+      flash.now[:alert] = TASK_NOT_CREATED
       render new_task_path
     end
   end
 
   def edit
     @task = Task.find_by(id: params[:id])
-    redirect_to tasks_path, notice: 'そのタスクないよ' unless @task
+    redirect_to tasks_path, notice: TASK_NOT_FOUND unless @task
   end
 
   def update
     @task = Task.find_by(id: params[:id])
-    redirect_to tasks_path, notice: 'そのタスクないよ' unless @task
+    redirect_to tasks_path, notice: TASK_NOT_FOUND unless @task
 
     # TODO: ステップ11までバリデーションは実装しません。
     if @task.update(task_params)
-      flash[:notice] = 'タスクを更新したよ'
+      flash[:notice] = TASK_UPDATED
       redirect_to task_url id: params[:id]
     else
-      flash.now[:alert] = 'タスクが更新できなかったよ'
+      flash.now[:alert] = TASK_NOT_UPDATED
       render edit_task_url
     end
   end
 
   def destroy
     task = Task.find_by(id: params[:id])
-    redirect_to tasks_path, notice: 'そのタスクないよ' unless task
+    redirect_to tasks_path, notice: TASK_NOT_FOUND unless task
 
-    task.destroy
-    redirect_to tasks_path, notice: 'タスクを削除したよ'
+    if task.destroy
+      redirect_to tasks_path, notice: TASK_DELETED
+    else
+      flash.now[:alert] = TASK_NOT_DELETED
+      render tasks_path
+    end
   end
 
   private
