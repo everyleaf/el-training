7cc16a51b173dcd85e52286ec1b80537d388cd2f
hanafuji <hanako.fujioka@rakuten.com>
2020-12-16 16:31:46 +0900

    マイグレーションファイルを編集。

--- gotodo/db/migrate/20201216070415_create_tasks.rb
+++ gotodo/db/migrate/20201216070415_create_tasks.rb
@@ -1,7 +1,15 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-
+      t.string :task_name
+      t.text :detail
+      t.text :location
+      t.decimal :lat
+      t.decimal :lng
+      t.integer :priority_no
+      t.integer :status_no
+      t.integer :user_id
+      t.datetime :end_date
       t.timestamps
     end
   end
