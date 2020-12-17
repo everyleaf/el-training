cc686e1d0ec326e4f727e27210200b1a8ce13c8b
mkLotto <hiroto.mukai@rakuten.com>
2020-12-17 16:30:53 +0900

    fix system spec

--- job_san/spec/system/application_spec.rb
+++ job_san/spec/system/application_spec.rb
@@ -3,17 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :system do
-  before { allow(Task).to receive(:all).and_raise(RuntimeError) }
+  context 'occur internal server error' do
+    before { allow(Task).to receive(:all).and_raise(RuntimeError) }
 
-  it 'render custom 500' do
-    get tasks_path
-    expect(response).to have_http_status :internal_server_error
-    expect(response.body).to have_content I18n.t('view.error.internal_server_error')
+    it 'render custom 500' do
+      get tasks_path
+      expect(response).to have_http_status :internal_server_error
+      expect(response.body).to have_content I18n.t('view.error.internal_server_error')
+    end
   end
 
-  it 'render custom 404' do
-    get "/#{SecureRandom.uuid}"
-    expect(response).to have_http_status :not_found
-    expect(response.body).to have_content I18n.t('view.error.routing_error')
+  context 'access not routed path' do
+    it 'render custom 404' do
+      get "/#{SecureRandom.uuid}"
+      expect(response).to have_http_status :not_found
+      expect(response.body).to have_content I18n.t('view.error.routing_error')
+    end
   end
 end
