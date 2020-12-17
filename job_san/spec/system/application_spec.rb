f08cf950b8d4c416f48332a19d9a0bc240c86f99
mkLotto <hiroto.mukai@rakuten.com>
2020-12-17 16:21:02 +0900

    add custom error page system spec

---
+++ job_san/spec/system/application_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :system do
+  before { allow(Task).to receive(:all).and_raise(RuntimeError) }
+
+  it 'render custom 500' do
+    get tasks_path
+    expect(response).to have_http_status :internal_server_error
+    expect(response.body).to have_content I18n.t('view.error.internal_server_error')
+  end
+
+  it 'render custom 404' do
+    get "/#{SecureRandom.uuid}"
+    expect(response).to have_http_status :not_found
+    expect(response.body).to have_content I18n.t('view.error.routing_error')
+  end
+end
