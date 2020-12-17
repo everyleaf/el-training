24784c5ed60540c483f83f78c648036b2aa57e76
mkLotto <hiroto.mukai@rakuten.com>
2020-12-17 14:31:33 +0900

    set jst timezone

--- job_san/config/application.rb
+++ job_san/config/application.rb
@@ -22,5 +22,8 @@ module JobSan
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
     config.i18n.available_locales = %i(ja)
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
