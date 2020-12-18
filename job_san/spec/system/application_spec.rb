41c40b8629c3633b9a42c09ba0a93f051cabffe1
mkLotto <hiroto.mukai@rakuten.com>
2020-12-18 13:14:33 +0900

    fix system specs

--- job_san/spec/system/application_spec.rb
+++ job_san/spec/system/application_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ApplicationController, type: :system do
     it 'render custom 500' do
       get tasks_path
       expect(response).to have_http_status :internal_server_error
-      expect(response.body).to have_content I18n.t('view.error.internal_server_error')
+      expect(response.body).to have_content 'そのアクセスはちょっと待って！'
     end
   end
 
@@ -17,7 +17,7 @@ RSpec.describe ApplicationController, type: :system do
     it 'render custom 404' do
       get "/#{SecureRandom.uuid}"
       expect(response).to have_http_status :not_found
-      expect(response.body).to have_content I18n.t('view.error.routing_error')
+      expect(response.body).to have_content 'そんなページないです。'
     end
   end
 end
