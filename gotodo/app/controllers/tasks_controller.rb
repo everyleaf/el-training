dfcb680787fc186f438e1868d8bc7cb513381264
hanafuji <hanako.fujioka@rakuten.com>
2020-12-21 12:15:19 +0900

    エラーを検知できるよう、`fetch`から`require`に変更。

--- gotodo/app/controllers/tasks_controller.rb
+++ gotodo/app/controllers/tasks_controller.rb
@@ -70,6 +70,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.fetch(:task, {}).permit(:task_name, :detail)
+      params.require(:task).permit(:task_name, :detail)
     end
 end
