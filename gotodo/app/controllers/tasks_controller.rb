ec4775a47ea1388032815caebc80df8763994a8a
hanafuji <hanako.fujioka@rakuten.com>
2020-12-21 13:46:04 +0900

    `rails g`で自動作成されたjson関連の記述やファイルを削除。

--- gotodo/app/controllers/tasks_controller.rb
+++ gotodo/app/controllers/tasks_controller.rb
@@ -30,10 +30,8 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -44,10 +42,8 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,7 +54,6 @@ class TasksController < ApplicationController
     @task.destroy
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
