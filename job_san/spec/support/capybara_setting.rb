9fad6ab9b775a65206586ce82c02d35e4c6b412c
mkLotto <hiroto.mukai@rakuten.com>
2020-12-23 18:15:53 +0900

    fix docker

--- job_san/spec/support/capybara_setting.rb
+++ job_san/spec/support/capybara_setting.rb
@@ -1,13 +1,25 @@
 require 'capybara/rspec'
 
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, options: {
-      browser: :remote,
-      url: ENV.fetch("SELENIUM_DRIVER_URL"),
-      desired_capabilities: :chrome
+Capybara.register_driver :remote_chrome do |app|
+  url = "http://chrome:4444/wd/hub"
+  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {
+      "args" => %w[no-sandbox headless disable-gpu window-size=1680,1050]
     }
-    Capybara.server_host = 'web'
-    Capybara.app_host='http://web'
+  )
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
+end
+
+RSpec.configure do |config|
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :remote_chrome
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.server_port = 8080
+    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
   end
 end
