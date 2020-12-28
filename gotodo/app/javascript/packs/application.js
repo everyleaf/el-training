1cf6589ed8b9c555d6fba34d8e484ecd3f874c81
hanafuji <hanako.fujioka@rakuten.com>
2020-12-28 17:16:39 +0900

    jQueryとBootstrapを利用するために諸々設定。

--- gotodo/app/javascript/packs/application.js
+++ gotodo/app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap";
+import "../stylesheets/application";
 
 Rails.start()
 Turbolinks.start()
--- gotodo/config/webpack/environment.js
+++ gotodo/config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+// jQuery, Bootstap JS
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+environment.loaders.prepend('erb', erb)
 module.exports = environment
