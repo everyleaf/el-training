92f925b8027ca1507a65ef6cb2884743f4b40309
hanafuji <hanako.fujioka@rakuten.com>
2021-01-04 12:12:15 +0900

    [エラー注意] save失敗時のフラッシュメッセージ用Rspecが書けない…

--- gotodo/spec/system/tasks_spec.rb
+++ gotodo/spec/system/tasks_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Tasks', type: :system do
     before do
       visit root_path
     end
-    it '登録済みタスクが表示されること' do 
+    it '登録済みタスクが表示されること' do
       expect(page).to have_content added_task.title
       expect(page).to have_content added_task.detail
     end
@@ -20,7 +20,7 @@ RSpec.describe 'Tasks', type: :system do
     before do
       visit task_path added_task.id
     end
-    it '登録済みタスクが表示されること' do 
+    it '登録済みタスクが表示されること' do
       expect(page).to have_content added_task.title
       expect(page).to have_content added_task.detail
     end
@@ -34,23 +34,42 @@ RSpec.describe 'Tasks', type: :system do
       fill_in '詳細', with: edited_task.detail
       click_button '更新する'
     end
-    it '編集したタスクが表示されること' do 
+    it '編集したタスクが表示されること' do
       expect(page).to have_content edited_task.title
       expect(page).to have_content edited_task.detail
     end
   end
 
   describe '#new' do
-    let(:new_task) { FactoryBot.create(:task, title: '美容院に行く', detail: 'ヘアサロン・ラクマ') }
-    before do
-      visit new_task_path
-      fill_in 'タスク名', with: new_task.title
-      fill_in '詳細', with: new_task.detail
-      click_button '登録する'
+    context '正常系' do
+      let(:new_task) { FactoryBot.create(:task, title: '美容院に行く', detail: 'ヘアサロン・ラクマ') }
+      before do
+        visit new_task_path
+        fill_in 'タスク名', with: new_task.title
+        fill_in '詳細', with: new_task.detail
+        click_button '登録する'
+      end
+      it '新規登録したタスクが表示されること' do
+        expect(page).to have_current_path task_path Task.last.id.to_s
+        expect(page).to have_content new_task.title
+        expect(page).to have_content new_task.detail
+      end
+      it 'Flashメッセージが表示されること' do
+        expect(page).to have_selector('#notice', text: '新しいタスクが登録されました！')
+      end
     end
-    it '新規登録したタスクが表示されること' do 
-      expect(page).to have_content new_task.title
-      expect(page).to have_content new_task.detail
+    context '異常系' do
+      let(:new_task) { FactoryBot.create(:task, title: nil, detail: nil) }
+      before do
+        visit new_task_path
+        fill_in 'タスク名', with: new_task.title
+        fill_in '詳細', with: new_task.detail
+        click_button '登録する'
+      end
+      it 'Flashメッセージが表示されること' do
+        expect(page).to have_current_path new_task_path
+        expect(page).to have_selector('#alert', text: 'タスクの作成に失敗しました。')
+      end
     end
   end
 
@@ -59,7 +78,7 @@ RSpec.describe 'Tasks', type: :system do
       visit root_path
       click_link nil, href: task_path(added_task), class: 'delete-link'
     end
-    it '削除した登録済みタスクが表示されないこと' do 
+    it '削除した登録済みタスクが表示されないこと' do
       expect(page).to have_no_content added_task.title
       expect(page).to have_no_content added_task.detail
     end
