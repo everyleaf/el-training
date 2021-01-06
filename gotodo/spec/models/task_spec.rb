142080f9bafbedf094f239ac7ea24ac37960ff00
hanafuji <hanako.fujioka@rakuten.com>
2021-01-06 15:30:45 +0900

    modelテストの文言修正。

--- gotodo/spec/models/task_spec.rb
+++ gotodo/spec/models/task_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe Task, type: :model do
   describe 'title (maximum:50)' do
     subject(:task) { FactoryBot.build(:task, title: title) }
 
-    shared_examples 'バリデーションを通過する' do
+    shared_examples 'バリデーションを通過すること' do
       it { is_expected.to be_valid }
     end
-    shared_examples 'バリデーションを通過しない' do
+    shared_examples 'バリデーションを通過しないこと' do
       it { is_expected.to be_invalid }
     end
 
     context '0文字の場合' do
       let(:title) { 'a' * 0 }
-      it_behaves_like 'バリデーションを通過しない'
+      it_behaves_like 'バリデーションを通過しないこと'
     end
     context '10文字の場合' do
       let(:title) { 'a' * 10 }
-      it_behaves_like 'バリデーションを通過する'
+      it_behaves_like 'バリデーションを通過すること'
     end
     context '50文字の場合' do
       let(:title) { 'a' * 50 }
-      it_behaves_like 'バリデーションを通過する'
+      it_behaves_like 'バリデーションを通過すること'
     end
     context '51文字の場合' do
       let(:title) { 'a' * 51 }
-      it_behaves_like 'バリデーションを通過しない'
+      it_behaves_like 'バリデーションを通過しないこと'
     end
   end
 end
