b3344bf89741860b9f7af39bde921ac11ec02667
hanafuji <hanako.fujioka@rakuten.com>
2021-01-06 15:08:48 +0900

    rubocopの指摘をもとに修正。

--- gotodo/app/helpers/tasks_helper.rb
+++ gotodo/app/helpers/tasks_helper.rb
@@ -3,18 +3,19 @@
 module TasksHelper
   def sortable(column)
     column = column.to_s
-    column_name = Task.human_attribute_name(column)
     asc = link_to '▲', sort: column, direction: 'asc'
     desc = link_to '▼', sort: column, direction: 'desc'
+    sortable = [Task.human_attribute_name(column)]
     if params[:sort] == column
-      if params[:direction] == 'asc'
-        sortable = column_name + desc
-      else
-        sortable = column_name + asc
-      end
+      sortable << if params[:direction] == 'asc'
+                    desc
+                  else
+                    asc
+                  end
     else
-      sortable = column_name + asc + desc
+      sortable << asc
+      sortable << desc
     end
-    sortable.html_safe
+    safe_join(sortable)
   end
 end
