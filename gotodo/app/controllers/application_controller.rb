32f79ad32df65b4b27eec598605c413651668e97
hanafuji <hanako.fujioka@rakuten.com>
2021-01-12 18:35:27 +0900

    デザイン変更。

--- gotodo/app/controllers/application_controller.rb
+++ gotodo/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :info, :warning, :danger
 end
--- gotodo/app/helpers/tasks_helper.rb
+++ gotodo/app/helpers/tasks_helper.rb
@@ -7,7 +7,7 @@ module TasksHelper
     asc = link_to '▲', hash.merge({ direction: 'asc' }), id: "#{column}_asc"
     desc = link_to '▼', hash.merge({ direction: 'desc' }), id: "#{column}_desc"
 
-    sortable = [Task.human_attribute_name(column)]
+    sortable = [Task.human_attribute_name(column) + '　']
     if params[:sort] == column
       sortable.concat(params[:direction] == 'asc' ? [desc] : [asc])
     else
