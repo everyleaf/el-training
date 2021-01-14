d119069ee0e893261d7364fb5396a751405c2415
hanafuji <hanako.fujioka@rakuten.com>
2021-01-14 12:57:56 +0900

    不要なコメントアウトを削除。

--- gotodo/app/models/task.rb
+++ gotodo/app/models/task.rb
@@ -14,24 +14,4 @@ class Task < ApplicationRecord
   scope :title_like, -> (title) { where('title LIKE ?', "%#{title}%") if title.present? }
   scope :status_is, -> (status) { where(status: status) if status.present? }
   scope :sorted, -> (sort, direction) { order("#{sort} #{direction}") if sort.present? && direction.present? }
-
-  # 備考：クラスメソッドバージョン
-  # scopeで良い場合はマージ前に削除します。
-  # def self.task_search(search_params)
-  #   title_like(search_params[:title])
-  #     .status_is(search_params[:status])
-  #     .sorted(search_params[:sort], search_params[:direction])
-  # end
-  #
-  # def self.title_like(title)
-  #   where('title LIKE ?', "%#{title}%") if title.present?
-  # end
-  #
-  # def self.status_is(status)
-  #   where('status = ?', Task.statuses[status]) if status.present?
-  # end
-  #
-  # def self.sorted(sort, direction)
-  #   order("#{sort} #{direction}") if sort.present? && direction.present?
-  # end
 end
