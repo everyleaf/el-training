4608cd571c1d951f6588e174862d9a0ca21dca9e
hanafuji <hanako.fujioka@rakuten.com>
2021-01-15 18:36:26 +0900

    マイグレーションがrollbackで失敗していたので修正。 `null:false`しているので、作成済みのtasksがあれば削除しておくこと。

--- gotodo/db/migrate/20210113065449_add_user_id_to_tasks.rb
+++ gotodo/db/migrate/20210113065449_add_user_id_to_tasks.rb
@@ -1,6 +1,5 @@
 class AddUserIdToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :tasks, :user, foreign_key: true, index: true
-    change_column_null :tasks, :user_id, false, 1
+    add_reference :tasks, :user, null: false, foreign_key: true, index: true
   end
 end
