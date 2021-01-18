cbfebba62b443a27d18a44fcc68ab0980394957c
hanafuji <hanako.fujioka@rakuten.com>
2021-01-18 11:25:15 +0900

    `# bundle exec rails g controller sessions new`

--- gotodo/config/routes.rb
+++ gotodo/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ gotodo/spec/requests/sessions_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Sessions", type: :request do
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/sessions/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
