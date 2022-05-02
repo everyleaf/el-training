257665c9a386c929de6a2a34f35263fb174a8da8
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-02 15:50:31 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 20_220_502_063_425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'task_name'
+    t.text 'abstract_text'
+    t.date 'start_date'
+    t.date 'deadline_date'
+    t.integer 'status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 end
