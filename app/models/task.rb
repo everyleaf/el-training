98f3218e258bd73059c4f9a01e08cd8c9d8a2e45
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-03 18:13:19 +0900

    タスクモデルの属性に最小限の存在バリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :task_name,     presence: true
+  validates :abstract_text, presence: true
+  # validates :start_date,    presence: true
+  validates :status,        presence: true
 end
