8067b7a164ec2f1f9caa998cb330a8c8ccb95ac3
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-03 19:02:55 +0900

    taskをresourcesに追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
+  # get 'tasks/new'
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
