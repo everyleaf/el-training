47c8293830a8971bf6a99db5b829198379ba433a
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-03 22:48:24 +0900

    taskのnewページのフォーム送信ボタンを有効にするために、createアクションを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,18 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params) #TODO strong parameter 
+    @task = Task.new(task_params) # TODO: strong parameter
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name,  :abstract_text,
+                                 :start_date, :deadline_date, :status)
   end
 end
