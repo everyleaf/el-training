a3418db667df8fbaa9d4c5e787953e290c3ef278
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-03 19:01:53 +0900

    タスクのnewアクションを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
   end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get tasks_new_url
     assert_response :success
   end
