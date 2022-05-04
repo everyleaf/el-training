137035a1c83c4878693c9b3212f1a203001b7c80
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-04 14:02:39 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/show'
-  get 'tasks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
