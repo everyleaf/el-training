743c335b304fdc4cf6428b3218de7f812210cc7e
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-04 13:53:59 +0900

    rubocopでフォーマット修正

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test 'should get show' do
     get tasks_show_url
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get tasks_index_url
     assert_response :success
   end
