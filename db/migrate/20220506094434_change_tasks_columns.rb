cc353ab59869daea7ee15fa5c12a04b7f1d0c24c
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-06 19:14:36 +0900

    取り掛かり開始日と進行状況にデフォルト値を設定

--- db/migrate/20220506094434_change_tasks_columns.rb
+++ db/migrate/20220506094434_change_tasks_columns.rb
@@ -6,9 +6,9 @@ class ChangeTasksColumns < ActiveRecord::Migration[7.0]
 
     change_column_null :tasks, :name,          false
     change_column_null :tasks, :description,   false
-    change_column_null :tasks, :start_date,    false
+    change_column_null :tasks, :start_date,    false, Time.zone.today
     change_column_null :tasks, :deadline_date, true
-    change_column_null :tasks, :progress,      false
+    change_column_null :tasks, :progress,      false, 0
 
     change_column_comment :tasks, :progress, from: nil, to: '0:未実行, 1:実行中, 2:完了'
   end
