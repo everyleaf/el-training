4c7a0a7b0dbe54929049a2f867a83890141e54d8
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-06 19:09:28 +0900

    変更後のカラム名が間違っていたのを修正

--- db/migrate/20220506094434_change_tasks_columns.rb
+++ db/migrate/20220506094434_change_tasks_columns.rb
@@ -1,6 +1,6 @@
 class ChangeTasksColumns < ActiveRecord::Migration[7.0]
   def change
-    rename_column :tasks, :task_name,     :task_name
+    rename_column :tasks, :task_name,     :name
     rename_column :tasks, :abstract_text, :description
     rename_column :tasks, :status,        :progress
 
