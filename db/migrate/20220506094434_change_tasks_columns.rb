9f2a44b53d619c6e56a4c037e5d363dfe89a0b3c
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-06 18:55:15 +0900

    taskのカラムに非nil制約を追加

--- db/migrate/20220506094434_change_tasks_columns.rb
+++ db/migrate/20220506094434_change_tasks_columns.rb
@@ -3,5 +3,12 @@ class ChangeTasksColumns < ActiveRecord::Migration[7.0]
     rename_column :tasks, :task_name,     :task_name
     rename_column :tasks, :abstract_text, :description
     rename_column :tasks, :status,        :progress
+
+    change_column_null :tasks, :name,          false
+    change_column_null :tasks, :description,   false
+    change_column_null :tasks, :start_date,    false
+    change_column_null :tasks, :deadline_date, true
+    change_column_null :tasks, :progress,      falsegit add .
+    
   end
 end
