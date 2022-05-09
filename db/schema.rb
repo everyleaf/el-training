df3bdfc36ea3bcd3cb91dc8cc1593997304a2962
KessaPassa <k.someya.gyao@gmail.com>
2022-05-09 11:27:27 +0900

    rubocop -a

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,5 @@
 
 ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+  enable_extension 'plpgsql'
 end
