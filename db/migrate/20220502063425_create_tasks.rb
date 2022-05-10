ed319dc0b7b19622c11fd3e17237b6147c404f5a
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-10 15:45:54 +0900

    taskモ デルのカラムの制約を変更

--- db/migrate/20220502063425_create_tasks.rb
+++ db/migrate/20220502063425_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string  :name,          null: false
-      t.text    :description,   null: false
-      t.date    :start_date,    null: false, default: Time.zone.today
+      t.text    :description
+      t.date    :start_date,    null: false
       t.date    :deadline_date
       t.integer :progress,      null: false, default: 0, comment: '0:未実行, 1:実行中, 2:完了'
 
