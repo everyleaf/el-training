3677ec97138e44427d2ebf9490aa9c65c0a88798
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-15 20:00:22 +0900

    taskのnewフォームとeditフォームを抽出

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,56 @@
 class TasksController < ApplicationController
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      flash[:success] = I18n.t 'task_create_success'
+    def new
+      @task = Task.new
+    end
+  
+    def create
+      @task = Task.new(task_params)
+      if @task.save
+        flash[:success] = I18n.t 'task_create_success'
+        redirect_to tasks_url
+      else
+        flash.now[:danger] = I18n.t 'task_create_failed'
+        render 'new'
+      end
+    end
+  
+    def show
+      @task = Task.find(params[:id])
+    end
+  
+    def index
+      @tasks = Task.all
+    end
+  
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
       redirect_to tasks_url
-    else
-      flash.now[:danger] = I18n.t 'task_create_failed'
-      render 'new'
+    
+    def edit
+      @task = Task.find(params[:id])
+    end
+  
+    def update
+      @task = Task.find(params[:id])
+      if @task.update(task_params)
+        flash[:success] = I18n.t 'task_update_success'
+        redirect_to @task
+      else
+        flash.now[:danger] = I18n.t 'task_update_failed'
+        render 'edit'
+      end
+    end
+  
+    private
+  
+    def task_params
+      params.require(:task).permit(:name,       :description,
+                                   :start_date, :necessary_days,
+                                   :progress,   :priority)
     end
   end
+<<<<<<< HEAD
 
   def show
     @task = find_task_with_err_handling(params[:id])
@@ -67,3 +105,6 @@ class TasksController < ApplicationController
     task
   end
 end
+=======
+  
+>>>>>>> 05353d6 (taskのnewフォームとeditフォームを抽出)
