27f5fe56eb3c98ea5d2fd6f7627494563afa3e83
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-15 20:46:56 +0900

    necessary_daysは1以上

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :name,           presence: true
   validates :start_date,     presence: true
-  validates :necessary_days, presence: true
+  validates :necessary_days, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :progress,       presence: true
   validates :priority,       presence: true
 
