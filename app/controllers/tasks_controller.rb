0261e45f78faa765a1d3223eb48b5a3ed93109bb
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-22 17:49:07 +0900

    flashメッセージをlacaleファイルに移動、I18n.tメソッドで呼び出し

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:success] = 'Task Created Successfully!'
+      flash[:success] = I18n.t 'create_success'
       redirect_to tasks_url
     else
       render 'new'
