47d871e97115889c81d42f367e977755fd59132b
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-23 14:12:41 +0900

    曜日モデルにコメントを追加

--- app/models/wday.rb
+++ app/models/wday.rb
@@ -1,6 +1,6 @@
 class Wday
   def initialize
-    @wday_all = %W(\u65E5 \u6708 \u706B \u6C34 \u6728 \u91D1 \u571F)
+    @wday_all = %W(\u65E5 \u6708 \u706B \u6C34 \u6728 \u91D1 \u571F) # ’日', '月',...,'土'
   end
 
   def get_wday_from_int(wday_int)
