be59db9de5501b25f04821b211bb6404cb511f48
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-23 14:06:11 +0900

    rubocopを適用

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 
 
