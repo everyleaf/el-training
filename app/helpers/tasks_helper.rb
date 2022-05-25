22e15919e6059b7fe026e56cb2fecab2c26fd3f7
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-25 13:26:11 +0900

    print_month_day_wday関数の引数にwdayを取らない実装

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,7 @@
 module TasksHelper
-  def print_month_day_wday(wday, date)
-    "#{date.month}/#{date.day}(#{wday.get_wday_from_int(date.wday)})"
+  def print_month_day_wday(date)
+    wday = Wday.new
+    wday_int = date.wday
+    "#{date.month}/#{date.day}(#{wday.get_wday_from_int(wday_int)})"
   end
 end
