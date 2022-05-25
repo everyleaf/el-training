cf00790a3786db556eae20ee9bd28845771d782f
smptmhr <tomohiro.shimpo@gmail.com>
2022-05-25 20:05:04 +0900

    曜日付き日付の出力をhelperメソッドで完結させる

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,8 +1,8 @@
 module TasksHelper
-    def print_month_day_wday(date)
-      wday_int = date.wday
-      wday_str = I18n.t('date.abbr_day_names')[wday_int]
-      "#{date.month}/#{date.day}(#{wday_str})"
-    end
+
+  def print_month_day_wday(date)
+    wday_int = date.wday
+    wday_str = I18n.t('date.abbr_day_names')[wday_int]
+    "#{date.month}/#{date.day}(#{wday_str})"
   end
   
