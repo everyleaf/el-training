099617b0d4bc6a0d44761114ba9071da1d3a9356
smptmhr <tomohiro.shimpo@gmail.com>
2022-06-09 14:47:58 +0900

    rubocopを適用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,39 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-
-before do
-  @task = Task.new(
-    name:           'test task',
-    description:    'this is a test task',
-    start_date:     Date.today,
-    necessary_days: 3,
-    progress:       '未着手',
-    priority:       '低'
-  )
-end
-
-  it "すべての項目が記入されていれば有効" do
-    expect(@task).to be_valid
+  it 'すべての項目が記入されていれば有効' do
+    task = build(:task)
+    expect(task).to be_valid
   end
 
-  it "descriptionが空でも有効" do
-    @task.description = nil
-    expect(@task).to be_valid
+  it 'descriptionが空でも有効' do
+    task = build(:task, description: nil)
+    expect(task).to be_valid
   end
 
-  it "nameが空だと無効" do
-    @task.name = nil
-    expect(@task).to be_invalid
+  it 'nameが空だと無効' do
+    task = build(:task, name: nil)
+    expect(task).to be_invalid
   end
 
-  it "necessary_daysが0だと無効" do
-    @task.necessary_days = 0
-    expect(@task).to be_invalid
+  it 'necessary_daysが0だと無効' do
+    task = build(:task, necessary_days: 0)
+    expect(task).to be_invalid
   end
 
-  it "necessary_daysが負の数だと無効" do
-    @task.necessary_days = -1
-    expect(@task).to be_invalid
+  it 'necessary_daysが負の数だと無効' do
+    task = build(:task, necessary_days: -1)
+    expect(task).to be_invalid
   end
 end
