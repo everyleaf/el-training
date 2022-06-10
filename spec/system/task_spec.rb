065ddf4513c3f6e88fa9e8f6e581f8a682d9f58e
smptmhr <tomohiro.shimpo@gmail.com>
2022-06-10 13:15:58 +0900

    コメントを修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Tasks', type: :system do
     # 作成リンクをクリック
     click_on 'タスクを作成'
 
-    # 郵便番号を入力
+    # フォームを埋める
     today = Time.zone.today
     fill_in 'Name',           with: 'sample task'
     fill_in 'Start date',     with: today
@@ -16,7 +16,7 @@ RSpec.describe 'Tasks', type: :system do
     choose  '未着手'
     choose  '低'
 
-    # 更新実行
+    # 作成実行
     click_button 'Create'
 
     # 正しくタスクが作成されていることを確認
