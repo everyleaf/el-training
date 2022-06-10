a7b8d3eb4bb536e05228d43b0b26472efd67b0d6
smptmhr <tomohiro.shimpo@gmail.com>
2022-06-10 16:44:53 +0900

    コメントを修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'Tasks', type: :system do
     # タスク一覧ページを表示
     visit tasks_path
 
-    # createのテストで作成したtaskを取得
+    # テスト用タスクを作成
     task = create(:task)
 
     # 詳細ページに移動
@@ -40,26 +40,26 @@ RSpec.describe 'Tasks', type: :system do
 
     # 正しい詳細ページに飛んだことを確認
     expect(page).to have_content 'test task'
-    expect(page).to have_link '編集'
-    expect(page).to have_button 'タスクを削除'
+    expect(page).to have_link    '編集'
+    expect(page).to have_button  'タスクを削除'
 
-    # 作成実行
+    # 編集ページに遷移
     click_link '編集'
 
     # 名前を空欄にして更新しようとすると失敗
-    fill_in 'Name', with: ''
+    fill_in      'Name', with: ''
     click_button 'Save changes'
     expect(page).to have_content "Name can't be blank"
 
     # 新しい名前にして更新
-    fill_in 'Name', with: 'updated task'
+    fill_in      'Name', with: 'updated task'
     click_button 'Save changes'
 
     # 更新成功
     expect(page).to have_content 'Task Updated Successfully!'
 
     # 詳細ページにいることを確認
-    expect(page).to have_link '編集'
+    expect(page).to have_link   '編集'
     expect(page).to have_button 'タスクを削除'
 
     # タスク名が更新されていることを確認
