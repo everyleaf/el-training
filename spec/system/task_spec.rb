094413d0aafecec85b10348dbee2215993704a8b
smptmhr <tomohiro.shimpo@gmail.com>
2022-06-10 16:10:36 +0900

    タスクの更新テストを作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  it 'タスクの作成' do # rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
+
+  # rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
+  it 'タスクの作成' do 
     # タスク一覧ページを作成
     visit tasks_path
 
@@ -25,5 +27,40 @@ RSpec.describe 'Tasks', type: :system do
     expect(page).to have_content '3'
     expect(page).to have_content '未着手'
     expect(page).to have_content '低'
+  end
+
+  it 'タスクの更新' do
+    # createのテストで作成したtaskを取得
+    task = create(:task)
+
+    # 詳細ページに移動
+    visit task_path(task)
+
+    # 正しい詳細ページに飛んだことを確認
+    expect(page).to have_content 'test task'
+    expect(page).to have_link '編集'
+    expect(page).to have_button 'タスクを削除'
+
+    # 作成実行
+    click_link '編集'
+
+    # 名前を空欄にして更新しようとすると失敗
+    fill_in 'Name',with: ''
+    click_button 'Save changes'
+    expect(page).to have_content "Name can't be blank"
+
+    # 新しい名前にして更新
+    fill_in 'Name',with: 'updated task'
+    click_button 'Save changes'
+
+    # 更新成功
+    expect(page).to have_content "Task Updated Successfully!"
+    
+    # 詳細ページにいることを確認
+    expect(page).to have_link '編集'
+    expect(page).to have_button 'タスクを削除'
+
+    # タスク名が更新されていることを確認
+    expect(page).to have_content 'updated task'
   end
 end
