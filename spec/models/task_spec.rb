b8412f68dd1ce5249efb0eb4a17092eef0ae4805
smptmhr <tomohiro.shimpo@gmail.com>
2022-06-22 12:17:44 +0900

    rubocop

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,37 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-
   context '全ての項目が記入されていれば' do
-    let(:task){ build(:task) }
+    let(:task) { build(:task) }
     it 'タスクは有効' do
       expect(task).to be_valid
     end
   end
 
   context 'descriptionが空でも' do
-    let(:task){ build(:task, description: nil) }
+    let(:task) { build(:task, description: nil) }
     it 'タスクは有効' do
       expect(task).to be_valid
     end
   end
 
   context 'nameが空だと' do
-    let(:task){ build(:task, name: nil) }
+    let(:task) { build(:task, name: nil) }
     it 'タスクは無効' do
       expect(task).to be_invalid
     end
   end
 
   context 'necessary_daysが0だと' do
-    let(:task){ build(:task, necessary_days: 0) }
+    let(:task) { build(:task, necessary_days: 0) }
     it 'タスクは無効' do
       expect(task).to be_invalid
     end
   end
 
   context 'necessary_daysが負の数だと' do
-    let(:task){ build(:task, necessary_days: -1) }
+    let(:task) { build(:task, necessary_days: -1) }
     it 'タスクは無効' do
       expect(task).to be_invalid
     end
