16a9e2e44534ce2bffdd3e17216640e0beb64512
smptmhr <tomohiro.shimpo@gmail.com>
2022-06-22 15:47:11 +0900

    contextとitの中の文章フォーマットを統一

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,38 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  context '全ての項目が記入されていれば' do
-    let(:task) { build(:task) }
-    it 'タスクは有効' do
-      expect(task).to be_valid
-    end
-  end
+  describe 'モデルのバリデーションテスト' do
 
-  context 'descriptionが空でも' do
-    let(:task) { build(:task, description: nil) }
-    it 'タスクは有効' do
-      expect(task).to be_valid
+    context '全てのパラメータが要素を持つとき' do
+      let(:task){ build(:task) }
+      it 'タスクは有効である' do
+        expect(task).to be_valid
+      end
     end
-  end
 
-  context 'nameが空だと' do
-    let(:task) { build(:task, name: nil) }
-    it 'タスクは無効' do
-      expect(task).to be_invalid
+    context 'descriptionのみがnilのとき' do
+      let(:task) { build(:task, description: nil) }
+      it 'タスクは有効である' do
+        expect(task).to be_valid
+      end
     end
-  end
 
-  context 'necessary_daysが0だと' do
-    let(:task) { build(:task, necessary_days: 0) }
-    it 'タスクは無効' do
-      expect(task).to be_invalid
+    context 'nameがnilのとき' do
+      let(:task) { build(:task, name: nil) }
+      it 'タスクは無効である' do
+        expect(task).to be_invalid
+      end
     end
-  end
 
-  context 'necessary_daysが負の数だと' do
-    let(:task) { build(:task, necessary_days: -1) }
-    it 'タスクは無効' do
-      expect(task).to be_invalid
+    context 'necessary_daysが0のとき' do
+      let(:task) { build(:task, necessary_days: 0) }
+      it 'タスクは無効である' do
+        expect(task).to be_invalid
+      end
+    end
+
+    context 'necessary_daysが負の数のとき' do
+      let(:task) { build(:task, necessary_days: -1) }
+      it 'タスクは無効である' do
+        expect(task).to be_invalid
+      end
     end
   end
 end
