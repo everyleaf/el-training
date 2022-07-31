9f52b9941ae2fdad344358b028b285636c992733
smptmhr <tomohiro.shimpo@gmail.com>
2022-07-31 21:19:35 +0900

    カテゴリモデルのバリデーションテストを作成

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  describe 'モデルのバリデーションテスト' do
+    context 'nameが空のとき' do
+      let(:category) { build(:category, name: '') }
+      it 'カテゴリは無効である' do
+        expect(task).to be_valid
+      end
+    end
+  end
 end
