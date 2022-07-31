f1ae66eca379cd031e15224524dc52954b2a5061
smptmhr <tomohiro.shimpo@gmail.com>
2022-07-31 15:02:22 +0900

    task categoryモデルを作成

---
+++ app/models/category.rb
@@ -0,0 +1,2 @@
+class Category < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_502_063_425) do
+ActiveRecord::Schema[7.0].define(version: 20_220_731_055_556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'tasks', force: :cascade do |t|
     t.string 'name', null: false
