d1fc6b12fa25e48df1335a625edbd65801586837
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-02 14:19:48 +0900

    カテゴリモデルの名前にユニーク性を追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
-  has_many :task, dependent: :restrict_with_error
-
+  has_many :task, dependent: :restrict_with_exception
   # TODO: ユーザモデルが追加されたら、nameのユニーク性のスコープをユーザに変更する
   validates :name, presence: true, uniqueness: true
 end
