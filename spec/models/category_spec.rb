48bb0c7b5692903297639ab5b0cca52c314f2bdb
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-03 14:54:24 +0900

    カテゴリ名のユニーク性についてのテスト

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Category, type: :model do
         expect(category).not_to be_valid
       end
     end
+
+    context 'カテゴリ名はユニーク' do
+      let!(:category) { create(:category, name: 'cat_1') }
+      it 'カテゴリは無効である' do
+        another_category = build(:category)
+        another_category.name = category.name
+        expect(another_category).not_to be_valid
+      end
+    end
   end
 end
