a8b8a6153c402ba287eb6bc4e8244fa9f52bc2cc
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 17:28:29 +0900

    ユーザindexページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
