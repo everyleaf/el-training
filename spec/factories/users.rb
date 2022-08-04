65116f129c4a1f0199f4b186c686fe0df4b6b9d8
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 15:46:32 +0900

    factorybotのユーザモデルにパスワードを設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { 'test user' }
     email { 'factory@example.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
