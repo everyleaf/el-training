e23905f9085837ddd42afc7862066ce44b4d4d58
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 16:32:20 +0900

    userのcreateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,3 +7,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to tasks_url
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
+  end
+
+end
