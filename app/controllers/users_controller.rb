7c7db13b2fd665a4d7377e07784def3df63bff7a
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 20:06:04 +0900

    文法ミスを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,12 +49,11 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name,
                                  :email,
                                  :password,
                                  :password_confirmation)
   end
-
 end
