fa5316ee22ab04e824731e55c25d410c4f30ec5b
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 15:52:43 +0900

    パスワードにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true,
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
