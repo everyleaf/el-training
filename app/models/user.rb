10a43cdb52137f3331da70222cd3cb3763ef46a9
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 15:39:48 +0900

    ユーザにパスワードを持たせるメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true,
                     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
