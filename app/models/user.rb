d3ff86fa59c4000e68c7bcbd7ab4f5eb08ae8d08
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-04 15:38:09 +0900

    メールアドレスに有効性バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name,  presence: true
-  validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX }
 end
