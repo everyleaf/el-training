a600f1b1c66857c3036843d324acb2d0b2145ba2
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-08 14:13:41 +0900

    関連付けのnull: falseを一行で記述

--- db/migrate/20220804095531_add_references_to_category.rb
+++ db/migrate/20220804095531_add_references_to_category.rb
@@ -1,7 +1,6 @@
 class AddReferencesToCategory < ActiveRecord::Migration[7.0]
   def change
-    add_reference         :categories, :user,    foreign_key: true
-    change_column_null    :categories, :user_id, false
+    add_reference         :categories, :user, foreign_key: true, null: false
     add_index             :categories, %i(name user_id), unique: true
   end
 end
