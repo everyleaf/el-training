04c2a949ecfe18663cf957c0d3f9875c3bff2c3f
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-08 09:18:17 +0900

    Model生成

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_08_05_081946) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title", limit: 128, default: "", null: false
+    t.text "content"
+    t.integer "user_id"
+    t.string "status", limit: 1, default: "1", null: false
+    t.string "label", limit: 64
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", limit: 128, default: "", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
