f1a194f46d590c7f02f400b034ce2c3049584b28
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-08 17:56:35 +0900

    コメント削除

--- myapp/app/helpers/task_helper.rb
+++ myapp/app/helpers/task_helper.rb
@@ -3,10 +3,6 @@ module TaskHelper
     def get_status_view(code)
         # 該当するコードを検索し、表示文言返却
         Task::STATUS_VIEW.keys.each do |key|
-            puts 'code::'
-            puts Task.statuses[code]
-            puts 'value[code]'
-            puts key
             if Task.statuses[code] == key then
                 return Task::STATUS_VIEW[key]
             end
