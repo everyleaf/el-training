a2ae5a7dd7ff138e44e15085f9c64af3a348ddf8
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-08 12:14:08 +0900

    タスクとカテゴリの関連付けを1つにまとめて再記述

--- db/migrate/20220731074230_add_references_to_task.rb
+++ db/migrate/20220731074230_add_references_to_task.rb
@@ -1,7 +1,8 @@
 class AddReferencesToTask < ActiveRecord::Migration[7.0]
   def change
-    add_reference         :tasks, :category,    foreign_key: true
-    change_column_null    :tasks, :category_id, false
-    change_column_default :tasks, :category_id, from: nil, to: Category.find_by(name: "未分類")
+    add_reference         :tasks, :category,
+                          foreign_key: true,
+                          null: false,
+                          default: Category.find_by(name: "未分類")
   end
 end
