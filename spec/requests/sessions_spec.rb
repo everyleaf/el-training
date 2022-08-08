a178e76cb58d42db3fdf6ee4a6eab19361acb907
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-08 16:36:59 +0900

    ログインパスをルーティング設定

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/sessions/new"
+RSpec.describe 'Sessions', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/sessions/new'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
