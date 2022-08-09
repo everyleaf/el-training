839260bb7831e3d3a3198eeada5313dd97afca71
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-09 15:42:53 +0900

    ログイン中のユーザ以外を表示・編集できないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -56,5 +58,22 @@ class UsersController < ApplicationController
                                  :email,
                                  :password,
                                  :password_confirmation)
+  end
+
+  # ユーザがログインしているか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please Login"
+      redirect_to login_url
+    end
+  end
+
+  # ログイン中のユーザが正しいか確認
+  def correct_user
+    user_to_edit = User.find(params[:id]) 
+    if user_to_edit != current_user
+      flash[:danger] = 'permission denied'
+      redirect_to root_url
+    end
   end
 end
