6538a56e1fde7ea0df9eb4e4686fa08ee7ef8877
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-09 14:38:13 +0900

    ログイン機能を最小限で実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = 'login_success'
+      redirect_to user
+    else
+      flash.now[:danger] = 'invalid_login'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
