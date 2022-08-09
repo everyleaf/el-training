329bcd6663dea98dfc3e60f01d34657ff3a2ec15
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-09 15:52:49 +0900

    ユーザ一覧をbefore_loginで保護

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :logged_in_user, only: [:edit, :update, :show, :index]
   before_action :correct_user,   only: [:edit, :update, :show]
   def new
     @user = User.new
