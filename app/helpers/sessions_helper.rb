8063626f279f3704f32fe5277349489a0e31b807
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-09 15:03:52 +0900

    現在ログイン中のユーザを返すメソッドを定義

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
+  # 引数のユーザでログイン
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログイン中のユーザ
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
 end
