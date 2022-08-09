2cf59cd4373ca16a514478874f505d0ede4308d7
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-09 14:54:12 +0900

    ログイン成功時にsessionにuseridを渡す

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       flash[:success] = I18n.t 'login_success'
       redirect_to user
     else
