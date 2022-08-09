f198cd7c2acf7b6b941b0beae4f97b352869a42c
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-09 16:18:06 +0900

    ユーザのdestroyアクション前にログインを要求

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  # ユーザがログインしているか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please Login"
+      redirect_to login_url
+    end
+  end
 end
