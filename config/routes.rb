bb1314d0096458f4e9a8819584dfbcebd3dd7621
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-10 14:32:11 +0900

    ログアウト時に「ログインしてください」の表示が出ないよう、rootをログインパスに変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users
   resources :categories, only: %i(index create destroy edit update)
-  root 'tasks#index'
+  root 'sessions#new'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
