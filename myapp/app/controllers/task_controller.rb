c1b475790168a944fb55001ad3deab78881bdee5
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-10 12:14:55 +0900

    不要ファイル削除

--- myapp/app/controllers/task_controller.rb
+++
@@ -1,8 +0,0 @@
-class TaskController < ApplicationController
-  def list
-    # タスク一覧オブジェクト取得
-    # Formのようなオブジェクトを作成すべきか？
-    # ただし、Formではないのでこれでよいと判断
-    @list_items = Task.joins(:user).select("tasks.title, tasks.label, users.name, tasks.status")
-  end
-end
