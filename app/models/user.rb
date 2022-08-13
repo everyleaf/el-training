86125526a6efd57b39d68179e77ba1fb9e3531c8
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-14 01:02:25 +0900

    不要な二重代入を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  VALID_EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name,     presence: true
   validates :password, presence: true,
