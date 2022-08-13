a6d44e9116f0b0baa2ea10bd826e830e166caa48
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-13 18:28:58 +0900

    タスクが属するデフォルトカテゴリの情報を、タスクモデルからカテゴリモデルに移譲

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
   TASK_DEFAULT_BELONG_NAME = '未分類'.freeze
-
   belongs_to :user
   has_many :task, dependent: :restrict_with_error
   validates :name, presence: true, uniqueness: { scope: :user }
