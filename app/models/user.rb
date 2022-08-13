2cf8c9fb819898f4adcccf19c2bda84df8ba22fb
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-13 19:11:16 +0900

    ユーザモデルの書き方を整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
   has_many :categories, dependent: :destroy
-  validates :name,  presence: true
-  VALID_EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true,
-                    format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8 }
+
+  VALID_EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
+  validates :name,     presence: true
+  validates :password, presence: true, 
+                       length: { minimum: 8 }
+  validates :email,    presence: true,
+                       uniqueness: true,
+                       format: { with: VALID_EMAIL_REGEX }
+
+  before_save { self.email = email.downcase }
 end
