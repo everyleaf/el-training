b0c41a36525ab6123636772269c9ddc0560042eb
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-16 14:25:23 +0900

    同じ挙動のメソッドを削除して、unlessを使用することで同様の動作を実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,8 +7,4 @@ class Category < ApplicationRecord
   def operation_prohibited?
     self.name == Category::TASK_DEFAULT_BELONG_NAME
   end
-
-  def operation_allowed?
-    !self.operation_prohibited?
-  end
 end
