c27f12c08432a00b2d4226c8008566f5d2e1fea3
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-16 19:28:16 +0900

    テスト環境のメーラーの設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhosthost:3000' }
 end
