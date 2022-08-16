8a911e31ccfbe9097af56c8b9290953eb53a89c7
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-16 20:15:55 +0900

    メールに記載されたリンクを踏むとアカウントを有効化

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,18 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated && user.authenticated?(params[:id])
+
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+
+      flash[:info] = I18n.t 'user_create_success'
+      Category.create(name: '未分類', user:)
+      log_in user
+      redirect_to tasks_url
+    else
+      flash[:danger] = I18n.t 'invalid_activation_link'
+      redirect_to root_url
+    end
+  end
 end
