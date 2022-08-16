fce1fc7adf9f628472469622f99d89107dc74481
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-16 16:12:15 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -42,10 +42,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_062518) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "remember_digest"
     t.string "activation_digest"
     t.boolean "activated", default: false
-    t.datetime "activated_at"
+    t.datetime "activated_at", precision: nil
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
