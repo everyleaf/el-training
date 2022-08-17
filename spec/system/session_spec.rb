064780978d4d650cc59d270569fd38e378119c48
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-17 21:15:11 +0900

    メール認証されていないユーザでログイン失敗のテスト

--- spec/system/session_spec.rb
+++ spec/system/session_spec.rb
@@ -26,5 +26,21 @@ RSpec.describe 'Categories', type: :system do
         expect(page).to have_content('ログイン情報が間違っています')
       end
     end
+
+    context '認証されていないユーザ情報を入力したとき' do
+      let!(:not_activated_user) { create(:user, activated: false) }
+      before do
+        fill_in 'session_email',    with: not_activated_user.email
+        fill_in 'session_password', with: not_activated_user.password
+        click_on 'Log in'
+      end
+      it 'ログインできない' do
+        expect(page).to     have_content ('アカウントを作成')
+        expect(page).not_to have_content ('タスク一覧')
+      end
+      it 'メールを確認するメッセージが表示される' do
+        expect(page).to have_content(I18n.t 'please_activate')
+      end
+    end
   end
 end
