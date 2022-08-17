1eb28e0bf38cf5ba547dd3b711c3338cb5e9921c
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-18 02:20:44 +0900

    ユーザ管理画面の詳細ページを作成

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,10 @@
 class AdminController < ApplicationController
+  include TasksHelper
   def index
     @users = User.preload(:categories, :tasks).all # preloadを使用してN+1問題に対応
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
