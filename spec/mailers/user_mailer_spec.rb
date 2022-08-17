bd48b73bf45003d2851738d2de0fdc7109122737
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-17 19:26:27 +0900

    アカウント有効化メールのテストを作成

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,31 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  describe 'account_activation' do
-    let(:mail) { UserMailer.account_activation }
+  describe 'アカウント有効化メール' do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.account_activation(user) }
 
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Account activation')
-      expect(mail.to).to eq(['to@example.org'])
+    it '宛先が正常である' do
+      expect(mail.subject).to eq(I18n.t 'account_authentication')
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(['from@example.com'])
     end
 
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
-    end
-  end
-
-  describe 'password_reset' do
-    let(:mail) { UserMailer.password_reset }
-
-    it 'renders the headers' do
-      expect(mail.subject).to eq('Password reset')
-      expect(mail.to).to eq(['to@example.org'])
-      expect(mail.from).to eq(['from@example.com'])
-    end
-
-    it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+    it 'アクティベーショントークンが含まれている' do
+      expect(mail.html_part.body.to_s).to include(user.activation_token)
     end
   end
 end
