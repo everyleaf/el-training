f457b0a8a99faf3c80b8c1192ade64af94e5d898
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-18 01:46:43 +0900

    adminコントローラを作成

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  get 'admin/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
