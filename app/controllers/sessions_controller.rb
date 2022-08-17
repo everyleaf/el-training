b3a288f40933a3beaa57b76d9539e2c6ee7b48fe
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-17 13:35:32 +0900

    有効化していないユーザではログインできない

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      flash[:success] = I18n.t 'login_success'
-      redirect_to tasks_url
+      if user.activated
+        log_in user
+        flash[:success] = I18n.t 'login_success'
+        redirect_to tasks_url
+        
+      else
+        flash[:warning] = I18n.t 'please_activate'
+        redirect_to root_url
+      end
+
     else
       flash.now[:danger] = I18n.t 'invalid_login_information'
       render 'new', status: :unprocessable_entity
