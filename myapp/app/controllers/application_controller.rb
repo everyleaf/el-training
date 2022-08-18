3ba00f2ddb42d09c61354812841783e964877a5d
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-19 08:55:11 +0900

    エラー画面対応※途中

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def render_404
+        render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+    end
+
+    def render_500
+        render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+    end
 end
