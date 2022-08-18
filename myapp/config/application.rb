218d593a6486623faaa8abfae74ac8b4d0181ca6
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-19 08:53:28 +0900

    TimeZone追加

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -11,6 +11,8 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
