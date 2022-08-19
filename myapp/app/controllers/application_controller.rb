072ead19c88b53b7bf8a7d9bad6f3d5cfe91cd6e
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-19 18:19:37 +0900

    rubocop対応

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   # 例外発生時のルーティング設定
   rescue_from Exception,                      with: :_render_500
   rescue_from ActionController::RoutingError, with: :_render_404
@@ -10,14 +9,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def _render_404(e = nil)
-      puts "Rendering 404 with excaption: #{e}" if e
-      render('errors/404.html', status: :not_found)
-    end
+  def _render_404(error = nil)
+    Rails.logger.debug { "Rendering 404 with excaption: #{error}" } if error
+    render('errors/404.html', status: :not_found)
+  end
 
-    def _render_500(e = nil)
-      puts "Rendering 500 with excaption: #{e}" if e
-      render('errors/500.html', status: :internal_server_error)
-    end
-
+  def _render_500(error = nil)
+    Rails.logger.debug { "Rendering 500 with excaption: #{error}" } if error
+    render('errors/500.html', status: :internal_server_error)
+  end
 end
