53235867b7461650904a162587406573028a0488
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-19 16:13:38 +0900

    エラーページ作成

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
-    def render_404
-        render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+
+  # 例外発生時のルーティング設定
+  rescue_from Exception,                      with: :_render_500
+  rescue_from ActionController::RoutingError, with: :_render_404
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  private
+
+    def _render_404(e = nil)
+      puts "Rendering 404 with excaption: #{e}" if e
+      render('errors/404.html', status: :not_found)
     end
 
-    def render_500
-        render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+    def _render_500(e = nil)
+      puts "Rendering 500 with excaption: #{e}" if e
+      render('errors/500.html', status: :internal_server_error)
     end
+
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   root to: 'tasks#index'
+
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
 end
