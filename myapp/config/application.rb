92653f300055e1d5c01bb3b6ad61842acf44174b
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-19 14:33:00 +0900

    日本語対応

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -8,15 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.time_zone = 'Asia/Tokyo'
+    # タイムゾーン設定
+    config.time_zone = 'Tokyo'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # 日本語設定
+    config.i18n.default_locale = :ja
+    # ファイルパス設定
+    config.i18n.load_path += Dir[Rails.root.join('path/to')]
+    config.i18n.load_path += Dir[Rails.root.join('path/to', '**').to_s]
   end
 end
 
