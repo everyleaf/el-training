5a5f2cc961378d60f232793bfb87418a6a1f555b
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-23 12:13:41 +0900

    メイラーのデフォルトコメントを削除

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,10 @@
 class UserMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
   def account_activation(user)
     @user = user
 
     mail to: user.email, subject: (I18n.t 'account_authentication')
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset
     @user = user
 
