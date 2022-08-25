73c5302c7295ac55a208eff20388279f3551b177
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-25 10:45:01 +0900

    カテゴリ未分類の使用では定数を呼び出す

--- db/migrate/20220810064342_change_task_category_default.rb
+++ db/migrate/20220810064342_change_task_category_default.rb
@@ -1,7 +1,7 @@
 class ChangeTaskCategoryDefault < ActiveRecord::Migration[7.0]
   def change
     change_column_default :tasks, :category_id,
-                          from: Category.find_by(name: '未分類'),
+                          from: Category.find_by(name: Category::DEFAULT_CREATED_CATEGORY),
                           to: nil
   end
 end
