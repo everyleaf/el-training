d488d556b504282550cf115b5d4fdfdbea4cefed
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-25 11:32:24 +0900

    使用していないパスワードリセットのメーラーを削除

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,10 +4,4 @@ class UserMailer < ApplicationMailer
 
     mail to: user.email, subject: (I18n.t 'account_authentication')
   end
-
-  def password_reset
-    @user = user
-
-    mail to: user.email, subject: (I18n.t 'reset_password')
-  end
 end
