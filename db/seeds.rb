bbff8e70e640b5886c6edcdc5242b6fb7206935f
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-28 19:20:51 +0900

    seedで複数ユーザ、複数タスクを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user = User.create(name: 'user_0',
-                   email: 'user_0@example.com',
-                   password: 'password',
-                   password_confirmation: 'password',
-                   activated: true,
-                   activated_at: Time.zone.now)
-Category.create(name: Category::DEFAULT_CREATED_CATEGORY, user:)
+5.times do |user_idx|
+  user = User.create(name: "seed_#{user_idx}",
+                     email: "seed_#{user_idx}@example.com",
+                     password: 'password',
+                     password_confirmation: 'password',
+                     activated: true,
+                     activated_at: Time.zone.now)
+  category = Category.create(name: Category::DEFAULT_CREATED_CATEGORY, user:)
+  5.times do |task_idx|
+    Task.create(name: "task_#{user_idx}_#{task_idx}",
+                category:,
+                start_date: Time.zone.now,
+                necessary_days: 5,
+                progress: 0,
+                priority: 0)
+  end
+end
