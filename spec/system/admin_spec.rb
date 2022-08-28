0935ced34893b9a865f5f3b132c86c24a34e6db6
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-28 22:08:55 +0900

    adminページからのユーザ削除のテスト

--- spec/system/admin_spec.rb
+++ spec/system/admin_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'admin page', type: :system do
   describe 'adminページトップ' do
+    let(:admin_user) {create(:user, name:  'admin',
+                                    email: 'admin@example.com')}
     before do
       5.times do |user_idx|
         create(:user, name: "user_#{user_idx}",
@@ -9,6 +11,7 @@ RSpec.describe 'admin page', type: :system do
                       password: 'password',
                       password_confirmation: 'password')
       end
+      login_as(admin_user)
       visit admin_index_path
     end
 
@@ -25,6 +28,27 @@ RSpec.describe 'admin page', type: :system do
         click_on 'user_0'
         expect(page).to     have_content 'user_0'
         expect(page).not_to have_content 'user_1'
+      end
+    end
+
+    context '削除ボタンを押したとき' do
+      it 'ユーザが削除される' do
+        id = User.first.id
+
+        # 削除するユーザ
+        expect(page).to have_content("user_#{id}")
+
+        # ユーザに割り当てられた削除ボタンを押す
+        find(".delete_user_#{id}").click 
+
+        # 確認のポップアップが表示される
+        expect(page.accept_confirm).to eq '本当に削除しますか?'
+
+        # 削除の成功のメッセージが表示される
+        expect(page).to     have_content('ユーザを削除しました')
+
+        # 削除されたユーザはadminページからも消える
+        expect(page).not_to have_content("user_#{id}")
       end
     end
   end
