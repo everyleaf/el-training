138dcfbaaa3f446e59308361b7b704433ef9c71a
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-28 22:23:38 +0900

    adminページ詳細のテストを作成

--- spec/system/admin_spec.rb
+++ spec/system/admin_spec.rb
@@ -28,17 +28,9 @@ RSpec.describe 'admin page', type: :system do
       end
     end
 
-    context 'ユーザ名を押したとき' do
-      it '詳細ページが表示される' do
-        click_on 'user_0'
-        expect(page).to     have_content 'user_0'
-        expect(page).not_to have_content 'user_1'
-      end
-    end
-
     context '削除ボタンを押したとき' do
       it 'ユーザが削除される' do
-        id = User.first.id
+        id = User.find_by(name: "user_0").id
 
         # 削除するユーザ
         expect(page).to have_content("user_#{id}")
@@ -54,6 +46,40 @@ RSpec.describe 'admin page', type: :system do
 
         # 削除されたユーザはadminページからも消える
         expect(page).not_to have_content("user_#{id}")
+      end
+    end
+  end
+
+  describe 'adminページ詳細' do
+    context 'adminページトップでユーザ名を押したとき' do
+      before do
+        visit admin_index_path
+      end
+
+      it '詳細ページが表示される' do
+        click_on 'user_0'
+        expect(page).to have_content 'ユーザ情報'
+        expect(page).to have_content 'カテゴリ一覧'
+        expect(page).to have_content 'タスク一覧'
+      end
+    end
+
+    context '削除ボタンを押したとき' do
+      it 'ユーザが削除される' do
+        # 削除するユーザ
+        id = User.find_by(name: "user_0").id
+
+        # 詳細ページへ移動
+        visit admin_path(id)
+
+        # ユーザに割り当てられた削除ボタンを押す
+        click_on "ユーザを削除"
+
+        # 確認のポップアップが表示される
+        expect(page.accept_confirm).to eq '本当に削除しますか?'
+
+        # 削除の成功のメッセージが表示される
+        expect(page).to have_content('ユーザを削除しました')
       end
     end
   end
