82ad497c52e9a09cffcdafc9c2863840e0c922d7
smptmhr <tomohiro.shimpo@gmail.com>
2022-08-31 18:16:28 +0900

    labelモデルを作成

---
+++ db/migrate/20220831080525_create_labels.rb
@@ -0,0 +1,8 @@
+class CreateLabels < ActiveRecord::Migration[7.0]
+  def change
+    create_table :labels do |t|
+      t.string  :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_064342) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_080525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_064342) do
     t.bigint "user_id", null: false
     t.index ["name", "user_id"], name: "index_categories_on_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -42,6 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_10_064342) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at", precision: nil
+    t.integer "role", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
