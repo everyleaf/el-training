94149b81ae6331f46185ad60f12c109949206fee
smptmhr <tomohiro.shimpo@gmail.com>
2022-09-01 18:32:47 +0900

    rubocop

--- db/migrate/20220831080525_create_labels.rb
+++ db/migrate/20220831080525_create_labels.rb
@@ -1,7 +1,7 @@
 class CreateLabels < ActiveRecord::Migration[7.0]
   def change
     create_table :labels do |t|
-      t.string  :name
+      t.string :name
       t.timestamps
     end
   end
--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { 'MyString' }
   end
 end
