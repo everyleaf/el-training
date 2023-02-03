5e1a267aaba31706950e9cc952e698dfcb2c0b4e
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-02-03 15:49:21 +0900

    tasksテーブルを追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_043748) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_061017) do
+  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "owner_id", null: false
+    t.string "status", null: false, comment: "[\"waiting\", \"doing\", \"completed\"]"
+    t.string "title", null: false
+    t.integer "priority", limit: 1, null: false, comment: "{1: \"high\", 2: \"middle\", 3: \"low\"}"
+    t.text "description", null: false
+    t.datetime "expires_at"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id", "expires_at"], name: "index_tasks_on_owner_id_and_expires_at"
+    t.index ["owner_id", "priority"], name: "index_tasks_on_owner_id_and_priority"
+    t.index ["owner_id", "status", "expires_at"], name: "index_tasks_on_owner_id_and_status_and_expires_at"
+    t.index ["owner_id", "status", "priority"], name: "index_tasks_on_owner_id_and_status_and_priority"
+    t.index ["owner_id", "status"], name: "index_tasks_on_owner_id_and_status"
+    t.index ["owner_id", "title"], name: "index_tasks_on_owner_id_and_title"
+    t.index ["owner_id"], name: "index_tasks_on_owner_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
