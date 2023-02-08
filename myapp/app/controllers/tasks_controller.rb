8644f69330697cffe977c788d298428a8db8a3ee
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-02-08 13:54:16 +0900

    docker-compose exec web rails generate scaffold_controller Task してviewファイルを修正

---
+++ myapp/app/controllers/tasks_controller.rb
@@ -0,0 +1,70 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update destroy ]
+
+  # GET /tasks or /tasks.json
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1 or /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks or /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1 or /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1 or /tasks/1.json
+  def destroy
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.fetch(:task, {})
+    end
+end
---
+++ myapp/test/controllers/tasks_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @task = tasks(:one)
+  end
+
+  test "should get index" do
+    get tasks_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_task_url
+    assert_response :success
+  end
+
+  test "should create task" do
+    assert_difference("Task.count") do
+      post tasks_url, params: { task: {  } }
+    end
+
+    assert_redirected_to task_url(Task.last)
+  end
+
+  test "should show task" do
+    get task_url(@task)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_task_url(@task)
+    assert_response :success
+  end
+
+  test "should update task" do
+    patch task_url(@task), params: { task: {  } }
+    assert_redirected_to task_url(@task)
+  end
+
+  test "should destroy task" do
+    assert_difference("Task.count", -1) do
+      delete task_url(@task)
+    end
+
+    assert_redirected_to tasks_url
+  end
+end
