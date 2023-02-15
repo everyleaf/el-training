51ce84feda45208e7c7a4daf1f812b83d36b856c
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-02-15 18:33:08 +0900

    タイムゾーンを変更

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -10,6 +10,8 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
     #
