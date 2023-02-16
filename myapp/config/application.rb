30d6130e187f48375c7ecde4b5572b18d296d56d
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-02-16 13:42:44 +0900

    I18nの設定を記述

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -10,8 +10,12 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
