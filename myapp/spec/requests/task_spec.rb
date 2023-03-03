fdee2d53379cc9a0e3f9d5e5643b8c6131bf0e24
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-03 15:15:31 +0900

    step10のPRを受けてテストを修正

--- myapp/spec/requests/task_spec.rb
+++ myapp/spec/requests/task_spec.rb
@@ -15,11 +15,11 @@ RSpec.describe 'Tasks', type: :request do
     context "When URL has argument 'sort'" do
       let!(:first_task) do
         create(:task, title: '1st', description: 'aaa', priority: 'low', status: 'waiting', user_id: user.id, 
-                      created_at: '2023/01/01 00:00')
+                      created_at: '2023/01/01 00:00', expires_at: '2023/02/02 00:00')
       end
       let!(:second_task) do
         create(:task, title: '2nd', description: 'bbb', priority: 'middle', status: 'waiting', user_id: user.id,
-                      created_at: '2023/01/02 00:00')
+                      created_at: '2023/01/02 00:00', expires_at: '2023/02/01 00:00')
       end
       context 'created_at_asc' do
         let(:params) do
@@ -45,34 +45,29 @@ RSpec.describe 'Tasks', type: :request do
           expect(response.body).to match(/#{second_task.title}[\s\S]*#{first_task.title}/)
         end
       end
-      context "When URL has argument 'sort'" do
-        let!(:tasks) { 11.times.map { create(:task, user_id: user.id) } }
-  
-        context 'expires_at_asc' do
-          let(:params) do
-            { sort: 'expires_at_asc' }
-          end
-  
-          it 'renders a successful response' do
-            get tasks_url, params: params
-  
-            expect(response).to have_http_status(:ok)
-            Task.all.slice(0..9).each { |task| expect(response.body).to include task.title.to_s }
-            expect(response.body).to include Task.all.last.title.to_s
-          end
+
+      context 'expires_at_asc' do
+        let(:params) do
+          { sort: 'expires_at_asc' }
         end
-        context 'expires_at_desc' do
-          let(:params) do
-            { sort: 'expires_at_desc' }
-          end
-  
-          it 'renders a successful response' do
-            get tasks_url, params: params
-  
-            expect(response).to have_http_status(:ok)
-            Task.all.reverse.slice(0..9).each { |task| expect(response.body).to include task.title.to_s }
-            expect(response.body).to include Task.all.reverse.last.title.to_s
-          end
+
+        it 'renders a successful response' do
+          get tasks_url, params: params
+
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to match(/#{second_task.title}[\s\S]*#{first_task.title}/)
+        end
+      end
+      context 'expires_at_desc' do
+        let(:params) do
+          { sort: 'expires_at_desc' }
+        end
+
+        it 'renders a successful response' do
+          get tasks_url, params: params
+
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to match(/#{first_task.title}[\s\S]*#{second_task.title}/)
         end
       end
     end
