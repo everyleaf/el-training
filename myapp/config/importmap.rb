011b38ef89369b3813ddb084ffd2b02706ef87e4
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-06 18:11:30 +0900

    bootstrapをinstall

--- myapp/config/importmap.rb
+++ myapp/config/importmap.rb
@@ -5,3 +5,6 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
--- myapp/config/initializers/assets.rb
+++ myapp/config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
