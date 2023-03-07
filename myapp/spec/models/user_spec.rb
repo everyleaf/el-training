92ccb7af1b5d6c135df30fd6b7a52e5184933c8e
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-07 13:54:55 +0900

    symbolを使用するよう記述を変更’

--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :bigint           not null, primary key
+#  deleted_at         :datetime
+#  email              :string(255)      not null
+#  encrypted_password :string(255)      not null
+#  name               :string(255)      not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#  index_users_on_name   (name)
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
