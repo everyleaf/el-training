ba361c5dc8fa2eb831f0e4cea69216e04a8a3233
“seungwoo-ryu” <seungwoo.ryu@rakuten.com>
2024-05-22 17:47:20 +0900

    Training Step7

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
