98ad134c9014f156b12b1a4696d4013ae3edc6a6
Sunao Uehara <uehara.sunao@gmail.com>
2024-08-13 11:28:05 +0900

    step 6, create tasks table
    add .DS_Store to gitignore

---
+++ myapp/db/migrate/20240813012823_change_title_to_tasks.rb
@@ -0,0 +1,11 @@
+class ChangeTitleToTasks < ActiveRecord::Migration[7.0]
+  def up
+    add_index :tasks, :title
+    change_column_null :tasks, :title, false
+  end
+
+  def down
+    remove_index :tasks, :title
+    change_column_null :tasks, :title, true
+  end
+end
