6aee02dffbd383742a9c7a1ac2e95805f59d9145
Sunao Uehara <uehara.sunao@gmail.com>
2024-08-13 11:28:05 +0900

    step 6, create tasks table
    add .DS_Store to gitignore

---
+++ myapp/db/migrate/20240809092656_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title, limit: 50
+      t.string :description, limit: 500
+
+      t.timestamps
+    end
+  end
+end
