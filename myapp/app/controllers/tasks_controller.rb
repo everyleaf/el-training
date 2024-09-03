7bb67f0ac3ce636ce94b4e62dc4e279b57239be3
Sunao Uehara <uehara.sunao@gmail.com>
2024-09-03 10:34:23 +0900

    step13 add status and search functionality

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -17,6 +17,7 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find_by(id: params[:id])
+    @status_map = get_status_map
     redirect_to error_path(404) if @task.nil?
   end
 
@@ -38,6 +39,8 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find_by(id: params[:id])
     redirect_to error_path(404) if @task.nil?
+
+    @status_map = get_status_map
   end
 
   def update
@@ -73,6 +76,15 @@ class TasksController < ApplicationController
       title: form_params[:task][:title],
       description: form_params[:task][:description],
       due_date_at: form_params[:task][:due_date_at],
+      status: form_params[:task][:status],
+   }
+  end
+
+  def get_status_map
+    {
+      STATUS_NOT_STARTED => I18n.t('status_not_started'),
+      STATUS_IN_PROGRESS => I18n.t('status_in_progress'),
+      STATUS_COMPLETED => I18n.t('status_completed'),
     }
   end
 end
